9c676ad43a5138d67ad176fba190b598f89453c6
Thales Moreira Carvalho <thales.comp@gmail.com>
2017-07-15 17:18:47 -0300

    Update NOTES about search terms

--- src/App.js
+++ src/App.js
@@ -22,12 +22,12 @@ class BooksApp extends React.Component {
               <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
               <div className="search-books-input-wrapper">
                 {/* 
-                  NOTES: Remember that the search from BooksAPI is limited to a particular set of search terms.
+                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
                   You can find these search terms here:
                   https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
                   
-                  However, BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find an specific author or title. Everything is limited by the search terms.
+                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+                  you don't find a specific author or title. Every search is limited by search terms.
                 */}
                 <input type="text" placeholder="Search by title or author"/>
                 
