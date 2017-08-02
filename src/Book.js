156e3cca20f602160327f7187ce8463c15d94cc8
Patrick Dan Lacuna <me@ptrklacuna.com>
2017-08-02 17:31:34 +0900

    feat: Display books from BooksAPI

---
+++ src/Book.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Book(props) {
+  console.log(props)
+  return (
+    <div className="book">
+      <div className="book-top">
+        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${props.book.imageLinks.smallThumbnail})`}}></div>
+        <div className="book-shelf-changer">
+          <select>
+            <option value="none" disabled>Move to...</option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None</option>
+          </select>
+        </div>
+      </div>
+      <div className="book-title">{props.book.title}</div>
+      <div className="book-authors">{props.book.authors.join(', ')}</div>
+    </div>
+  )
+}
+
+export default Book
