f1de43e68806a7811e4f80af75c1b45f80dd7919
Patrick Dan Lacuna <me@ptrklacuna.com>
2017-08-05 20:09:25 +0900

    fix: Move 'select' attr of book-shelf-changer
    - Changed 'selected' from <option> tags to 'value" attr in <select> tag

--- src/Book.js
+++ src/Book.js
@@ -8,13 +8,15 @@ function Book(props) {
       <div className="book-top">
         <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})`}}></div>
         <div className="book-shelf-changer">
-          <select onChange={ event => {
-            updateBook(book.id, event.target.value);
-          }}>
+          <select value={book.shelf}
+            onChange={ event => {
+              updateBook(book.id, event.target.value);
+            }
+          }>
             <option value="none" disabled>Move to...</option>
-            <option value="currentlyReading" selected={book.shelf === 'currentlyReading' ? 'selected' : ''}>Currently Reading</option>
-            <option value="wantToRead" selected={book.shelf === 'wantToRead' ? 'selected' : ''}>Want to Read</option>
-            <option value="read" selected={book.shelf === 'read' ? 'selected' : ''}>Read</option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
             <option value="none">None</option>
           </select>
         </div>
