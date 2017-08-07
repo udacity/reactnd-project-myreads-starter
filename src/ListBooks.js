be4805ac1052518fcf23052feff768f9f87c87cb
Pasquale Guglielmi <guglielmipasquale@yahoo.it>
2017-08-07 15:57:55 +0100

    move: <Book /> component code into a separate Book.js file

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,32 +1,8 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
+import Book from './Book'
 
-/* boos need imageUrl title author shelf*/
-
-function Book(props) {
-    return (
-        <li>
-            <div className="book">
-                <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${props.imageURL})` }}></div>
-                    <div className="book-shelf-changer">
-                        <select>
-                            <option value="none" disabled>Move to...</option>
-                            <option value="currentlyReading">Currently Reading</option>
-                            <option value="wantToRead">Want to Read</option>
-                            <option value="read">Read</option>
-                            <option value="none">None</option>
-                        </select>
-                    </div>
-                </div>
-                <div className="book-title">{props.title}</div>
-                <div className="book-authors">{props.author}</div>
-            </div>
-        </li>
-    )
-}
-/* ListBooks need books*/
 class ListBooks extends Component {
     render() {
         return (
