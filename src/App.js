ac045feb7dcb429a2405646ebeb1a098a49c596a
Pasquale Guglielmi <guglielmipasquale@yahoo.it>
2017-08-07 15:49:47 +0100

    add: books to <BooksApp /> state and pass it to <ListBooks /> as a prop to render the new <Book /> component

--- src/App.js
+++ src/App.js
@@ -7,13 +7,21 @@ import { Route } from 'react-router-dom'
 
 class BooksApp extends React.Component {
   state = {
+    books: []
+  }
+
+  componentDidMount = () => {
+    BooksAPI.getAll().then((books) => {this.setState({ books })
+    })
   }
 
   render() {
     return (
       <div className="app">
           <Route path='/search' component={SearchPage}/>
-          <Route exact path='/' component={ListBooks}/>
+          <Route exact path='/' render={() => (
+            <ListBooks books={this.state.books}/>
+          )}/>
       </div>
     )
   }
