0e6ba9b1d1e80408227d64e4074e7f4310bc4da1
Sreeram Sama <sama.sreeram13@gmail.com>
2017-08-14 19:19:15 -0700

    show list of books matching the search

--- src/Book.js
+++ src/Book.js
@@ -4,11 +4,12 @@ import BookShelfChanger from './BookShelfChanger'
 class Book extends Component {
   render() {
     const { book, toChangeShelf } = this.props
+    const bookInShelf = book.shelf ? true : false
     return (
       <div className="book">
         <div className="book-top">
           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${(book.imageLinks || {}).smallThumbnail})` }}></div>
-          <BookShelfChanger book={book} toChangeShelf={toChangeShelf}/>
+          <BookShelfChanger book={book} toChangeShelf={toChangeShelf} bookInShelf={bookInShelf}/>
         </div>
         <div className="book-title">{book.title}</div>
         {(book.authors || []).map((author) => (
--- src/BookShelfChanger.js
+++ src/BookShelfChanger.js
@@ -7,13 +7,15 @@ class BookShelfChanger extends Component {
   handleSubmit = (e) => {
     e.preventDefault()
     if(this.props.toChangeShelf)
-      this.props.toChangeShelf(this.props.book, e.target.value)
+      this.props.toChangeShelf(this.props.book, e.target.value, this.props.bookInShelf)
   }
   render() {
-    const { book, toChangeShelf } = this.props
+    const { book, bookInShelf } = this.props
+    // bookInShelf is used to track if book is in shelf or book is in searched results
+    const pickerValue = bookInShelf ? book.shelf : "none"
     return (
       <div className="book-shelf-changer">
-        <select value = {book.shelf} onChange={this.handleSubmit}>
+        <select value={pickerValue} onChange={this.handleSubmit}>
           <option value="none" disabled>Move to...</option>
           <option value="currentlyReading">Currently Reading</option>
           <option value="wantToRead">Want to Read</option>
