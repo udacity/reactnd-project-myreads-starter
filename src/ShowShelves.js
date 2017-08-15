e4e902ea647bbfba2cb937e8c4c21fdeff87e852
Sreeram Sama <sama.sreeram13@gmail.com>
2017-08-14 19:20:49 -0700

    remove unnecessary onNavigate fuction call

--- src/ShowShelves.js
+++ src/ShowShelves.js
@@ -3,13 +3,8 @@ import { Link } from 'react-router-dom'
 import CurrentlyReadingShelf from './CurrentlyReadingShelf'
 import WantToReadShelf from './WantToReadShelf'
 import ReadShelf from './ReadShelf'
-import * as BooksAPI from './BooksAPI'
 
 class ShowShelves extends Component {
-  state = {
-    shelf: []
-  }
-
   render() {
     const { books, toChangeShelf } = this.props
     const currentlyReadingBooks = books.filter((book) => (book.shelf === 'currentlyReading'))
@@ -26,7 +21,7 @@ class ShowShelves extends Component {
           </div>
         </div>
         <div className="open-search">
-          <Link to='/add' onClick={this.props.onNavigate}></Link>
+          <Link to='/add'></Link>
         </div>
       </div >
     )
