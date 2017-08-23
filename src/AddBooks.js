85d2f8ef9bb2cbc06314b146526dd34c40e98f80
Sreeram Sama <sama.sreeram13@gmail.com>
2017-08-22 17:28:05 -0700

    change the origin of searchBooks call to correct multiple re-renders and search requests

--- src/AddBooks.js
+++ src/AddBooks.js
@@ -8,14 +8,14 @@ class AddBooks extends Component {
     query: ''
   }
   updateQuery = (query) => {
+    if (query) {
+      this.props.toSearchBooks(query)
+    }
     this.setState({ query: query.trim() })
   }
   render() {
     const { query } = this.state
     const { books, toSearchBooks, toChangeShelf } = this.props
-    if (query) {
-      toSearchBooks(query)
-    }
     return (
       <div className="search-books">
         <div className="search-books-bar">
@@ -27,7 +27,6 @@ class AddBooks extends Component {
             <input type="text" placeholder="Search books by title or author"
               value={query}
               onChange={(event) => debounce(5000, this.updateQuery(event.target.value))} />
-            {query}
           </div>
         </div>
         <div className="search-books-results">
