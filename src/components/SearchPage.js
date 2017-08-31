17501e7138e06f305eba3b663bc3058300736164
Danilo Rossi <contact@danilorossi.me>
2017-08-31 18:34:12 +0200

    refactor: Split App markup into Home and Search
    Navigation is still implemented through App.js state.

---
+++ src/components/SearchPage.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+
+class SearchPage extends React.Component {
+
+  render() {
+    return (
+      <div className="search-books">
+        <div className="search-books-bar">
+          <a className="close-search" onClick={() => this.props.navigateToHomePage()}>Close</a>
+          <div className="search-books-input-wrapper">
+            {/*
+              NOTES: The search from BooksAPI is limited to a particular set of search terms.
+              You can find these search terms here:
+              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+              you don't find a specific author or title. Every search is limited by search terms.
+            */}
+            <input type="text" placeholder="Search by title or author"/>
+
+          </div>
+        </div>
+        <div className="search-books-results">
+          <ol className="books-grid"></ol>
+        </div>
+      </div>
+    )
+  }
+}
+
+SearchPage.propTypes = {
+  navigateToHomePage: PropTypes.func.isRequired,
+};
+
+export default SearchPage;
