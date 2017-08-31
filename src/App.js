a36576ff6a28e3789771bba5026580e892af7d51
James Weis <jweis@revampagency.com>
2017-08-31 16:50:16 -0400

    Changed header

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ class BooksApp extends React.Component {
         ) : (
           <div className="list-books">
             <div className="list-books-title">
-              <h1>MyReads</h1>
+              <h1>James Reads</h1>
             </div>
             <div className="list-books-content">
               <div>
