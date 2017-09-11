ac550c04c742cdf1bf7cfc8cb41749fdc87ec5a4
Sarah Kurisu <sarah.kurisu@gmail.com>
2017-09-10 21:53:26 -0400

    docs: add comment to show what's left for app

--- src/App.js
+++ src/App.js
@@ -6,14 +6,11 @@ import BookWrapper from './BookWrapper'
 import './App.css'
 
 class BooksApp extends React.Component {
+  /**
+   * TODO: Create controlled select box on Book component
+   */
+  
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    // showSearchPage: false
     currentlyReading: [],
     wantToRead: [],
     read: [],
