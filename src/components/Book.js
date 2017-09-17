c972fbbdb3684709b61120a3e46e10db58b5d05b
Tiago Almeida <tyagow@hotmail.com.br>
2017-09-17 14:44:33 -0300

    refactor: Adicionando props ao componente Book

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,14 +1,19 @@
-
-
 import React, {Component} from "react";
+import PropTypes from 'prop-types'
 
 class Book extends Component {
+
+    static propTypes = {
+      url: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      author: PropTypes.string.isRequired
+    }
     render () {
         return (
 
             <div className="book">
               <div className="book-top">
-                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${ this.props.url }")` }}></div>
                 <div className="book-shelf-changer">
                   <select>
                     <option value="none" disabled>Move to...</option>
@@ -19,8 +24,8 @@ class Book extends Component {
                   </select>
                 </div>
               </div>
-              <div className="book-title">To Kill a Mockingbird</div>
-              <div className="book-authors">Harper Lee</div>
+              <div className="book-title">{this.props.title}</div>
+              <div className="book-authors">{this.props.author}</div>
             </div>
 
         )
--- src/components/Bookshelf.js
+++ src/components/Bookshelf.js
@@ -7,25 +7,33 @@ class Bookshelf extends Component {
     render () {
         return (
             <div>
-              <div className="bookshelf">
-                <h2 className="bookshelf-title">Currently Reading</h2>
-                <div className="bookshelf-books">
-                  <ol className="books-grid">
-                    <li>
-                      <Book/>
-                    </li>
-                    <li>
-                      <Book/>
+                <div className="bookshelf">
+                    <h2 className="bookshelf-title">Currently Reading</h2>
+                    <div className="bookshelf-books">
+                        <ol className="books-grid">
+                            <li>
+                                <Book
+                                    title='To Kill a Mockingbird'
+                                    url='http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api'
+                                    author='Harper Lee'
+                                />
+                            </li>
+                            <li>
+                                <Book
+                                    title='Enders Game'
+                                    url='http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api'
+                                    author='Orson Scott Card'
+                                />
 
-                    </li>
-                  </ol>
+                            </li>
+                        </ol>
+                    </div>
                 </div>
-              </div>
 
 
-              <div className="open-search">
-                <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-              </div>
+                <div className="open-search">
+                    <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+                </div>
             </div>
         )
     }
