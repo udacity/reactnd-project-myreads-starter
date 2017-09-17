5789290a1b9d9edeba14929f7249da59533e4a56
Helio Ricardo <helio.ricardo@bsconsult.com.br>
2017-09-16 21:57:51 -0300

    refactor: Componentize Books and loop through shelves

---
+++ src/Book.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Book = props => {
+  return (
+    <div className="book">
+      <div className="book-top">
+        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${props.image})` }}></div>
+            <div className="book-shelf-changer">
+              <select>
+                <option value="none" disabled>Move to...</option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+              </select>
+            </div>
+        </div>
+      <div className="book-title">{ props.title }</div>
+      <div className="book-authors">{ props.authors }</div>
+    </div>
+  )
+}
+
+export default Book
