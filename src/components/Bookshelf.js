d6cae54375a9ba81b563de5097d6d899ea6807e8
Tiago Almeida <tyagow@hotmail.com.br>
2017-09-17 16:55:02 -0300

    refactor: Bookshelf renderiza dinamicamente cada Book

--- src/components/Bookshelf.js
+++ src/components/Bookshelf.js
@@ -5,37 +5,34 @@ import Book from "./Book";
 import PropTypes from 'prop-types'
 
 class Bookshelf extends Component {
-    static propTypes = {
-        shelfName: PropTypes.string.isRequired
-    }
-    render () {
-        return (
+  static propTypes = {
+    shelfName: PropTypes.string.isRequired
+  }
+  state = {
+    books: []
+  }
+  render () {
+    const books = this.props.books
+    return (
+      <div className="bookshelf">
+          <h2 className="bookshelf-title">{this.props.shelfName}</h2>
+          <div className="bookshelf-books">
+              <ol className="books-grid">
+                {books.map((book) => (
+                  <li key={book.title}>
+                      <Book
+                        title={book.title}
+                        smallThumbnail={book.imageLinks.smallThumbnail}
+                        authors={book.authors}
+                      />
+                  </li>
+                ))}
+              </ol>
+          </div>
+      </div>
 
-            <div className="bookshelf">
-                <h2 className="bookshelf-title">{this.props.shelfName}</h2>
-                <div className="bookshelf-books">
-                    <ol className="books-grid">
-                        <li>
-                            <Book
-                                title='To Kill a Mockingbird'
-                                url='http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api'
-                                author='Harper Lee'
-                            />
-                        </li>
-                        <li>
-                            <Book
-                                title='Enders Game'
-                                url='http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api'
-                                author='Orson Scott Card'
-                            />
-
-                        </li>
-                    </ol>
-                </div>
-            </div>
-
-        )
-    }
+  )
+  }
 }
 
 export default Bookshelf
