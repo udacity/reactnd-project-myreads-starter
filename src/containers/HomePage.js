98c33ff6c3c96ae26237df433edad304e8e43e87
Tiago Almeida <tyagow@hotmail.com.br>
2017-09-19 19:15:04 -0300

    refactor: Substituindo tag <a> por <Link>

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import { Link } from "react-router-dom";
 import * as BooksAPI from "../BooksAPI";
 import Bookshelf from "../components/Bookshelf";
 
@@ -38,7 +39,7 @@ class HomePage extends Component {
                     </div>
                 </div>
                 <div className="open-search">
-                    <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+                    <Link to="/search">Add a book</Link>
                 </div>
             </div>
         )
