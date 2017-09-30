83b663b261adc9785952b07c763ac5defd7e174c
Tiago Almeida <tyagow@hotmail.com.br>
2017-09-30 15:32:05 -0300

    refactor: mudando tag <a> para <Link/> para voltar para home

--- src/containers/SearchPage.js
+++ src/containers/SearchPage.js
@@ -4,17 +4,10 @@ import PropTypes from 'prop-types'
 
 
 class SearchPage extends Component {
-    // static propTypes = {
-    //     onMoveBook: PropTypes.func.isRequired,
-    //     shelves: PropTypes.array.isRequired,
-    //     books: PropTypes.array.isRequired
-    // };
-    //
-    // getShelfBooks(key) {
-    //     return this.props.books.filter((book) => {
-    //         return book.shelf === key;
-    //     })
-    // }
+    static propTypes = {
+        onMoveBook: PropTypes.func.isRequired,
+        shelves: PropTypes.array.isRequired,
+    };
 
 
     render () {
@@ -23,7 +16,7 @@ class SearchPage extends Component {
         return (
             <div className="search-books">
                 <div className="search-books-bar">
-                    <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+                    <Link className="close-search" to="/">Close</Link>
                     <div className="search-books-input-wrapper">
                         <input type="text" placeholder="Search by title or author"/>
                     </div>
