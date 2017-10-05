2c842c5fec8c70811438f3dffed98037e6280dc9
Tiago Almeida <tyagow@hotmail.com.br>
2017-10-05 20:27:54 -0300

    refactor: Criando novo metodo para achar um livro dentro do array books (que contem meus livros)

--- src/App.js
+++ src/App.js
@@ -15,12 +15,35 @@ class BooksApp extends React.Component {
         };
 
     }
+    findBookInShelves = (book) => {
+        let bookListToReturn = this.state.books.filter((bookInShelf) => {
+                                  return book.id === bookInShelf.id
+                          })
+        if (bookListToReturn !== null) {
+            if (bookListToReturn.length >= 1) {
+
+                return bookListToReturn[0]
+            }
+        }
+
+      return false
+    }
     searchBooks = (e) => {
         const query = e.target.value.trim()
         BooksAPI.search(query, 40).then((results) => {
             if (results.length) {
-                console.log(results)
-                this.setState({results: results})
+                this.setState({
+                    results: results.map((resultBook) => {
+                        let bookInShelf = this.findBookInShelves(resultBook)
+                        if (bookInShelf) {
+                            if (bookInShelf !== undefined) {
+                                resultBook.shelf = bookInShelf.shelf
+                            }
+                        }
+                        return resultBook
+                    })
+                })
+
             }
             else {
                 console.log('Error results')
@@ -32,6 +55,8 @@ class BooksApp extends React.Component {
     onMoveBook = (book, shelf) => {
         console.log(book, shelf)
         BooksAPI.update(book, shelf).then((bookId) =>{
+
+
             this.setState((prevState) => {
                 return {books: prevState.books.map((findBook) => {
                     if (book.id === findBook.id) {
