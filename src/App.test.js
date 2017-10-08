cdceff49ff03ae03bfbeaef3552927fecce10f7a
Tiago Almeida <tyagow@hotmail.com.br>
2017-10-08 12:58:33 -0300

    test: Criando test para funcoes updateBook e pushBook

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import React from "react"
-import App from "./App"
+import App, {updateBooks, pushBook} from "./App"
 import {shallow} from "enzyme"
 import Adapter from 'enzyme-adapter-react-15'
 import * as Enzyme from "enzyme"
@@ -11,8 +11,23 @@ import * as Enzyme from "enzyme"
 Enzyme.configure({ adapter: new Adapter() })
 const fetchMock = require('fetch-mock')
 fetchMock.get('*', {});
-it('renders without crashing', () => {
-    expect(shallow(<App />)).toMatchSnapshot()
+// it('renders without crashing', () => {
+//     expect(shallow(<App />)).toMatchSnapshot()
+// })
+
+it('updateResults update the results array if book.shelf in', () => {
+    var results = [{id: 1, shelf:"wantToRead"}]
+    const book = {id: 1, shelf:"currentRead"}
+
+    results = updateBooks(results, book)
+    expect(results[0].shelf).toBe("currentRead");
+
 })
 
+it('pushBook check if book is in current array given and push it to the array if not in', () => {
+    var books = [{id: 1}]
+    const book = {id: 2, shelf:"currentRead"}
+    const results = pushBook(books, book)
+    expect(results[1].id).toBe(2);
 
+})
