a03d59b022372079dbabd6ccd32e6b8a47806406
Tiago Almeida <tyagow@hotmail.com.br>
2017-10-08 09:03:40 -0300

    refactor: transformando Book Class Component em Functional component

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,36 +1,27 @@
-import React, {Component} from "react";
-import PropTypes from 'prop-types'
+import React from "react";
 
-class Book extends Component {
-
-  static propTypes = {
-    book: PropTypes.object.isRequired,
-    onMoveBook: PropTypes.func.isRequired
-  }
-
-
-  render () {
-    const book = this.props.book
+function Book(props) {
+    const book = props.book
+    const onMoveBook = props.onMoveBook
     return (
-      <div className="book">
-        <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${ book.imageLinks.smallThumbnail }")` }}></div>
-          <div className="book-shelf-changer">
-            <select value={book.shelf}  onChange={(event) => this.props.onMoveBook(book, event.target.value)}>
-              <option value="none" disabled>Move to...</option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="wantToRead" >Want to Read</option>
-              <option value="read" >Read</option>
-              <option value="none">None</option>
-            </select>
+        <div className="book">
+          <div className="book-top">
+            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${ book.imageLinks.smallThumbnail }")` }}></div>
+            <div className="book-shelf-changer">
+              <select value={book.shelf}  onChange={(event) => onMoveBook(book, event.target.value)}>
+                <option value="none" disabled>Move to...</option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead" >Want to Read</option>
+                <option value="read" >Read</option>
+                <option value="none">None</option>
+              </select>
+            </div>
           </div>
+          <div className="book-title">{book.title}</div>
+          <div className="book-authors">{book.authors}</div>
         </div>
-        <div className="book-title">{book.title}</div>
-        <div className="book-authors">{book.authors}</div>
-      </div>
 
     )
-  }
 }
 
 export default Book
