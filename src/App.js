a647c5aa0dc0a613d2aeacac1ae6eaeee604e2dd
joseteodoro <jteodoro.dev@gmail.com>
2017-10-12 11:33:07 -0300

    defining book using properties

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
+import Book from './components/book'
 
 class BooksApp extends React.Component {
   state = {
@@ -56,24 +57,7 @@ class BooksApp extends React.Component {
                   <div className="bookshelf-books">
                     <ol className="books-grid">
                       {this.state.reading.map((book) => {
-                        return <li>
-                          <div className="book">
-                            <div className="book-top">
-                              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.cover})` }}></div>
-                              <div className="book-shelf-changer">
-                                <select>
-                                  <option value="none" disabled>Move to...</option>
-                                  <option value="currentlyReading">Currently Reading</option>
-                                  <option value="wantToRead">Want to Read</option>
-                                  <option value="read">Read</option>
-                                  <option value="none">None</option>
-                                </select>
-                              </div>
-                            </div>
-                            <div className="book-title">{book.title}</div>
-                            <div className="book-authors">{book.author}</div>
-                          </div>
-                          </li>
+                        return <li key={book.title}><Book book={book} /></li>
                         })
                       }
                     </ol>
