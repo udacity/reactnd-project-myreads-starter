f30749166b456e266fcdff8a7e85e13994894c0c
Ivan <ivan7707@hotmail.com>
2017-10-29 19:33:42 -0400

    feat: moving shelves for the books works

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import { Route } from 'react-router-dom'
 import './App.css'
 import SearchBooks from './SearchBooks'
@@ -7,20 +7,69 @@ import ListBooks from './ListBooks'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    books: []
   }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books })
+    })
+
+  }
+
+
+  updateBook = (book, shelf) => {
+    
+  this.setState((state) => {
+      const results = this.state.books.filter(currentbook => currentbook.id === book.id);
+      results[0].shelf = shelf
+
+  }
+    )
+
+BooksAPI.update(book, shelf)
+
+}
+
+//     Hey @ivan7707 just to get you moving in the right dirction think about using a filter 
+// to look through this.state.books and return when this.state.books === books.id
+
+// That will return to you the book that needs to be udpated
+
+// I'm just heading out but if you haven't figured it out or had any more help by the time I log 
+// back in tomorrow I'll be happy to help
+
+// Or you could use map to look through this.state.books and update the object where the ID's are the same
+
+//     )
+// }
+
+  //     this.setState((state) => {
+  //         Books: {book.shelf}
+
+  //     // })
+  //     console.log('TEST UPDATE', book.shelf, shelf)
+
+
+      
+            
+              
+  //     //   }))
+
+  //     BooksAPI.update(book, shelf)
+
+  
+  
 
   render() {
     return (
        <div className="app">
         <Route exact path='/' render={() => (
-          <ListBooks />
+          <ListBooks
+            onUpdateBook={this.updateBook}
+            books={this.state.books}
+
+           />
         )}/>
 
         <Route exact path='/search' render={() => (
