e65b74190c64f72dba4b4c739edc2d421409fff5
ovsundal <osundal83@gmail.com>
2017-11-04 14:00:30 +0100

    feat: add query state for SearchBar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,23 +1,35 @@
 import React, {Component} from 'react'
 
-//component for listing books
-class ListBooks extends Component {
+/**
+ * Component for book searching input field
+ */
+class SearchBar extends Component {
+
+    state = {
+        query: ''
+    };
+
+    updateQuery = (query) => {
+        this.setState({query: query.trim()})
+    };
 
     render() {
         return(
             <div className="search-books">
                 <div className="search-books-bar">
+
                     <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
                     <div className="search-books-input-wrapper">
-                        {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+                        {
 
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                        <input type="text" placeholder="Search by title or author"/>
+                            JSON.stringify(this.state)}
+                        }
+                        <input
+                            type="text"
+                            placeholder="Search by title or author"
+                            value={this.state.query}
+                            onChange={(event) => this.updateQuery(event.target.value)}
+                        />
 
                     </div>
                 </div>
@@ -33,4 +45,4 @@ class ListBooks extends Component {
 
 
 
-export default ListBooks;
+export default SearchBar;
