5a76d2846231a8bb43b0c7f5aa54e974caf2c674
ovsundal <osundal83@gmail.com>
2017-11-05 21:54:11 +0100

    fix: book author and title are now correct

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -45,16 +45,15 @@ class SearchBar extends Component {
                             value={this.state.query}
                             onChange={(event) => this.updateQuery(event.target.value)}
                         />
-
                     </div>
                 </div>
                 <div className="search-books-results">
                     <ol className="books-grid">
-                        {showingBooks.map((books) => (
-                        <li key={books.id}>
+                        {showingBooks.map((book) => (
+                        <li key={book.id}>
                             <div className="book">
                                 <div className="book-top">
-                                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage:`url(${books.imageLinks.smallThumbnail}` }}>
+                                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage:`url(${book.imageLinks.smallThumbnail}` }}>
                                         <div className="book-shelf-changer">
                                             <select>
                                                 <option value="none" disabled>Move to...</option>
@@ -66,8 +65,8 @@ class SearchBar extends Component {
                                         </div>
                                     </div>
                                 </div>
-                                <div className="book-title">To Kill a Mockingbird</div>
-                                <div className="book-authors">Harper Lee</div>
+                                <div className="book-title">{`${book.title}`}</div>
+                                <div className="book-authors">{`${book.authors}`}</div>
                             </div>
                         </li>
                         ))}
