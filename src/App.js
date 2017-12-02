45672ae2b5d6bd7e31f181719298d50851730906
edisonfm <edisonferraz@gmail.com>
2017-12-02 20:19:28 -0200

    chore: add eslint and prettier

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import React from 'react'
-import SearchBook from './components/SearchBooks'
-import ListBooks from './components/ListBooks'
-import { Route } from 'react-router-dom'
+import React from 'react';
+import SearchBook from './components/SearchBooks';
+import ListBooks from './components/ListBooks';
+import { Route } from 'react-router-dom';
 // import * as BooksAPI from './api/BooksAPI'
-import './App.css'
+import './App.css';
 
 class BooksApp extends React.Component {
-  state = { }
-
   render() {
     return (
       <div className="app">
         <Route path="/" exact component={ListBooks} />
         <Route path="/search" component={SearchBook} />
       </div>
-    )
+    );
   }
 }
 
-export default BooksApp
+export default BooksApp;
