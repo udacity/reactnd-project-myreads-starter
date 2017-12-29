04cb9e2dd74826e28054a48e042a8f8e35b2b620
Samir Marin <marin.samir@gmail.com>
2017-12-28 23:10:52 -0800

    factor out BooksGrid to seperate component

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,50 +1,15 @@
 import React, { Component } from 'react'
+import BooksGrid from './BooksGrid'
 
 class BookShelf extends Component {
   render() {
     return (
       <div className="bookshelf">
         <h2 className="bookshelf-title">{ this.props.title }</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            <li>
-              <div className="book">
-                <div className="book-top">
-                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
-                  <div className="book-shelf-changer">
-                    <select>
-                      <option value="none" disabled>Move to...</option>
-                      <option value="currentlyReading">Currently Reading</option>
-                      <option value="wantToRead">Want to Read</option>
-                      <option value="read">Read</option>
-                      <option value="none">None</option>
-                    </select>
-                  </div>
-                </div>
-                <div className="book-title">To Kill a Mockingbird</div>
-                <div className="book-authors">Harper Lee</div>
-              </div>
-            </li>
-            <li>
-              <div className="book">
-                <div className="book-top">
-                  <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")' }}></div>
-                  <div className="book-shelf-changer">
-                    <select>
-                      <option value="none" disabled>Move to...</option>
-                      <option value="currentlyReading">Currently Reading</option>
-                      <option value="wantToRead">Want to Read</option>
-                      <option value="read">Read</option>
-                      <option value="none">None</option>
-                    </select>
-                  </div>
-                </div>
-                <div className="book-title">Ender's Game</div>
-                <div className="book-authors">Orson Scott Card</div>
-              </div>
-            </li>
-          </ol>
-        </div>
+        <BooksGrid
+          books={[{title: "To Kill a Mockingbird", author: "Harper Lee", id: 1}, 
+          {title: "Ender's Game", author: "Orsons Scott Card", id: 2}]}
+        />
       </div>
     )
   }
