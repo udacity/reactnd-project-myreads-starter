04555702ee52fa072b8a04d2e5b4075b5c373f09
Samir Marin <marin.samir@gmail.com>
2017-12-31 14:00:50 -0800

    Use react-router to move between search and myRead page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <BrowserRouter><App /></BrowserRouter>,
+  document.getElementById('root')
+)
