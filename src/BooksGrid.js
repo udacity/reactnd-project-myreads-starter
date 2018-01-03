b831c1577e8328d9418cc6d640e757aed3db0462
Samir Marin <marin.samir@gmail.com>
2018-01-02 16:57:32 -0800

    Add remove book icon

--- src/BooksGrid.js
+++ src/BooksGrid.js
@@ -5,6 +5,9 @@ class BooksGrid extends Component {
   handleChange(event, book) {
     this.props.onUpdateBookShelf(book, event.target.value)
   }
+  removeBook(event, book) {
+    this.props.onUpdateBookShelf(book, event)
+  }
 
   render() {
     return (
@@ -17,12 +20,14 @@ class BooksGrid extends Component {
                   <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: book.imageLinks ? "url(" + book.imageLinks.thumbnail + ")" : null }}></div>
                   <div className="book-shelf-changer">
                     <select value={book.shelf ? book.shelf : "none"} onChange={(event) => this.handleChange(event, book)}>
-                      <option value="none" disabled>Move to...</option>
+                      <option value="non" disabled>Move to...</option>
                       <option value="currentlyReading">Currently Reading</option>
                       <option value="wantToRead">Want to Read</option>
                       <option value="read">Read</option>
                       <option value="none">None</option>
                     </select>
+                  </div>
+                  <div className="remove-book-from-library" onClick={(event) => this.removeBook("none", book)}>
                   </div>
                 </div>
                 <div className="book-title">{book.title}</div>
