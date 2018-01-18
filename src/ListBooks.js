9c93c164daa7c046b9ba471dd1442eb0f54dcb5b
ozgunbalaban <ozgunbalaban@yahoo.com>
2018-01-18 18:56:47 +0800

    feat: implement with arrays of books

---
+++ src/ListBooks.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class ListBooks extends Component {
+  render() {
+    return (
+      <ol className="books-grid">
+        {this.props.books.map((book) => (
+          <li key={book.id}>
+            <div className="book">
+              <div className="book-top">
+                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+                  <div className="book-shelf-changer">
+                    <select>
+                      <option value="none" disabled>Move to...</option>
+                      <option value="currentlyReading">Currently Reading</option>
+                      <option value="wantToRead">Want to Read</option>
+                      <option value="read">Read</option>
+                      <option value="none">None</option>
+                    </select>
+                  </div>
+                </div>
+                <div  className="book-title">{book.title}</div>
+                <div className="book-authors">{book.author}</div>
+              </div>
+          </li>
+        ))}
+      </ol>
+    )
+
+  }
+}
+
+export default ListBooks
