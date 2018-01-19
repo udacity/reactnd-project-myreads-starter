6e8bdc8f50067248eba6b1501333e1a3b779cb67
ozgunbalaban <ozgunbalaban@yahoo.com>
2018-01-19 15:04:13 +0800

    feat: Add Thumbnails

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -67,7 +67,13 @@ return (
           <li key={book.id}>
             <div className="book">
               <div className="book-top">
-                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+                <div className="book-cover">
+                    <img src={book.imageLinks.thumbnail}
+                    style={{ width: 128,
+                             height: 193}}>
+                    </img>
+                    </div>
+
                   <div className="book-shelf-changer">
                     <select>
                       <option value="none" disabled>Move to...</option>
@@ -78,8 +84,8 @@ return (
                     </select>
                   </div>
                 </div>
-                <div  className="book-title">{book.title}</div>
-                <div className="book-authors">{book.author}</div>
+                <div className="book-title">{book.title}</div>
+                <div className="book-authors">{book.authors[0]}</div>
               </div>
           </li>
         ))}
