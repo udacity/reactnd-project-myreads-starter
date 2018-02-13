3895b6d22650eed90b271e9d0e3d33f3528be46f
icloud <yinaiyun@live.cn>
2018-02-14 00:22:50 +0800

    完成列表，跳转

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function HeaderTitle(props) {
+    return ( 
+    <div className="list-books-title">
+       <h1>{props.title}</h1>
+     </div>
+  )
+}
+
+
+export default HeaderTitle
