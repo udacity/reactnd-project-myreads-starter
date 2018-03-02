a0dd5dc0ba0681e96551bae63491743e0cbb8eb4
Carlos Amaral <CarlosAmaral@users.noreply.github.com>
2018-03-03 00:38:14 +0400

    Creating components and adding state functionality

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import {Layout} from 'antd';
 import './App.css'
+import * as BooksAPI from './BooksAPI'
 import HeaderComponent from "./components/Header";
 import {Link, Route} from "react-router-dom";
 import {Button, Tabs} from "antd";
 import CurrentlyReading from "./components/CurrentlyReading";
 import WantToRead from "./components/WantToRead";
 import Read from "./components/Read";
+import Search from "./components/Search";
+import OurSelection from "./components/OurSelection";
 
 const operations = <Button>Extra Action</Button>;
 const TabPane = Tabs.TabPane;
+const {Content} = Layout;
 
 class BooksApp extends React.Component {
     state = {
+
+
         /**
          * TODO: Instead of using this state variable to keep track of which page
          * we're on, use the URL in the browser's address bar. This will ensure that
@@ -27,51 +33,50 @@ class BooksApp extends React.Component {
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */
-        showSearchPage: false
+        books: []
+
     };
+
+    componentDidMount() {
+        BooksAPI.getAll().then((books) => {
+            console.log(books, "books");
+            this.setState({books})
+        });
+    }
 
     render() {
         return (
             <div className="app">
-                <Route path="/search" exact render={() => (
-                    <div className="search-books">
-                        <div className="search-books-bar">
-                            <Link className="close-search" to="/">Close</Link>
-                            <div className="search-books-input-wrapper">
-                                {}
-                                <input type="text" placeholder="Search by title or author"/>
-
-                            </div>
-                        </div>
-                        <div className="search-books-results">
-                            <ol className="books-grid"></ol>
-                        </div>
-                    </div>
-                )}/>
                 <HeaderComponent/>
-                <Tabs tabBarExtraContent={operations} type="card">
-                    <TabPane tab="Currently Reading" key="1">
-                        <CurrentlyReading/>
-                    </TabPane>
-                    <TabPane tab="Want to Read" key="2">
-                        <WantToRead/>
-                    </TabPane>
-                    <TabPane tab="Read" key="3">
-                        <Read/>
-                    </TabPane>
-                    <TabPane tab="Ratings" key="4">Ratings</TabPane>
-                </Tabs>
-                <div className="list-books">
-                    <div className="list-books-content">
-                        <div>
+                <Route path="/search" exact render={() => (
+                    <Search/>
+                )}/>
+                <Route path="/" exact render={() => (
+                    <Layout style={{margin: '24px 16px 0', marginTop: '20px'}}>
+                        <Content>
+                            <Tabs tabBarExtraContent={operations} type="card">
+                                <TabPane tab="Our Selection" key="1">
+                                    <OurSelection  books={this.state.books}/>
+                                </TabPane>
+                                <TabPane tab="Currently Reading" key="2">
+                                    <CurrentlyReading/>
+                                </TabPane>
+                                <TabPane tab="Want to Read" key="3">
+                                    <WantToRead/>
+                                </TabPane>
+                                <TabPane tab="Read" key="4">
+                                    <Read/>
+                                </TabPane>
+                                <TabPane tab="Ratings" key="5">Ratings</TabPane>
+                            </Tabs>
 
-                        </div>
-                    </div>
-                    <div className="open-search">
-                        <Link to="/search">Add a book</Link>
-                    </div>
+                        </Content>
+                    </Layout>
+                )}/>
+                <div className="open-search">
+                    <Link to="/search">Add a book</Link>
                 </div>
-                )}
+
             </div>
         )
     }
---
+++ src/components/Search/index.js
@@ -0,0 +1,25 @@
+import React, {Component} from "react";
+import {Link} from "react-router-dom";
+
+class Search extends Component {
+
+    render() {
+        return (
+            <div className="search-books">
+                <div className="search-books-bar">
+                    <Link className="close-search" to="/">Close</Link>
+                    <div className="search-books-input-wrapper">
+                        {}
+                        <input type="text" placeholder="Search by title or author"/>
+
+                    </div>
+                </div>
+                <div className="search-books-results">
+                    <ol className="books-grid"></ol>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Search;
