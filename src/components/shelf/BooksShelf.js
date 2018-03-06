c2fc892b1fc746d4a9d0af417bf6c7961ea78efc
Larissa Roder <larissaroder@gmail.com>
2018-03-05 21:34:38 -0300

    criação da estante de livros, divisão da estante pelo status de leitura

---
+++ src/components/shelf/BooksShelf.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+import BookShelf from './BookShelf'
+
+class BooksShelf extends Component {
+
+	static propTypes = {
+		books: PropTypes.array.isRequired,
+		changeShelf: PropTypes.func.isRequired,
+	}
+
+	render() {
+		const { books, changeShelf } = this.props
+		const shelfTypes = [{ type: 'currentlyReading', title: 'Currently Reading' },
+							{ type: 'wantToRead',  title: 'Want to Read' },
+							{ type: 'read', title: 'Read'}]
+		return (
+			<div className="list-books-content">
+				{shelfTypes.map((shelf, index) =>  {
+					const shelfBooks = books.filter( book => book.shelf === shelf.type)
+					return  (
+						<div className="bookshelf" key={index}>
+							<h2 className="bookshelf-title">{ shelf.title }</h2>
+							<div className="bookshelf-books">
+								<BookShelf
+								books={ shelfBooks }
+								changeShelf={ changeShelf }
+								/>
+							</div>
+						</div> 
+					)
+        })}
+			</div>
+		);
+	}
+}
+export default BooksShelf;
