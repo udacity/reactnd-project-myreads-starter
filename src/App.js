81f9a7078840873199a341fab0bdea180a28f3ae
Ian Johnson <ian@IAN-JOHNSON-MBA-TECH-2017.local>
2018-03-09 16:05:44 -0800

    first

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    showSearchPage: false
+
+     books: []
+    //showSearchPage: false
   }
 
   render() {
