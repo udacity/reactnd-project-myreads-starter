793967490d70b74b927b5dc4a6dd8ce116d0d313
Allan Carlos <allancarlosdeveloper@gmail.com>
2018-03-15 17:03:03 -0300

    style

--- src/App.js
+++ src/App.js
@@ -6,36 +6,12 @@ import SearchBook from "./SearchBook";
 import "./App.css";
 
 const App = () => (
-  <Switch>
-    <Route exact path='/' component={ListBooks} />
-    <Route path='/search' component={SearchBook} />
-  </Switch>
+  <div className="app">
+    <Switch>
+      <Route exact path='/' component={ListBooks} />
+      <Route path='/search' component={SearchBook} />
+    </Switch>
+  </div>
 );
 
 export default App
-
-
-
-
-
-// import React from "react";
-// // import * as BooksAPI from './BooksAPI'
-// import ListBooks from "./ListBooks";
-// import SearchBook from "./SearchBook";
-// import "./App.css";
-
-// class BooksApp extends React.Component {
-//   state = {};
-
-//   render() {
-//     return (
-//       <div className="app">
-//         <SearchBook />
-
-//         <ListBooks />
-//       </div>
-//     );
-//   }
-// }
-
-// export default BooksApp;
