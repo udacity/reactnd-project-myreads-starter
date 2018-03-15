febc9033937b88f82e9635eadb427ce70ae92949
Allan Carlos <allancarlosdeveloper@gmail.com>
2018-03-15 16:47:01 -0300

    refactor: Separate SearchBook component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,16 @@
-import React from "react"
+import React from "react";
 // import * as BooksAPI from './BooksAPI'
-import ListBooks from "./ListBooks"
-import "./App.css"
+import ListBooks from "./ListBooks";
+import SearchBook from "./SearchBook";
+import "./App.css";
 
 class BooksApp extends React.Component {
-  state = {
-  };
+  state = {};
 
   render() {
     return (
       <div className="app">
-        <div className="search-books">
-          <div className="search-books-bar">
-            <a
-              className="close-search"
-              onClick={() => this.setState({ showSearchPage: false })}
-            >
-              Close
-            </a>
-            <div className="search-books-input-wrapper">
-              {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-              <input type="text" placeholder="Search by title or author" />
-            </div>
-          </div>
-          <div className="search-books-results">
-            <ol className="books-grid" />
-          </div>
-        </div>
+        <SearchBook />
 
         <ListBooks />
       </div>
