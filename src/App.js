0d4571510e8bbcc09337bd6c7773f26fff0bacaa
Allan Carlos <allancarlosdeveloper@gmail.com>
2018-03-15 22:37:21 -0300

    feat: Implement states

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
-// // import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from "./BooksAPI";
 import ListBooks from "./ListBooks";
 import SearchBook from "./SearchBook";
 import "./App.css";
 
-const App = () => (
-  <div className="app">
-    <Switch>
-      <Route exact path='/' component={ListBooks} />
-      <Route path='/search' component={SearchBook} />
-    </Switch>
-  </div>
-);
+class App extends Component {
+  state = {
+    books: []
+  };
 
-export default App
+  componentDidMount() {
+    BooksAPI.getAll().then(book => {
+      this.setState({ books: book });
+    });
+  }
+
+  render() {
+    const { books } = this.state;
+
+    return (
+      <div className="app">
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => <ListBooks books={books} />}
+          />
+          <Route path="/search" component={SearchBook} />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,13 +1,21 @@
-import React, { Component } from "react";
+import React, { Component } from "react"
+import PropTypes from "prop-types"
 import { Link } from 'react-router-dom'
 
 class ListBooks extends Component {
+  static propTypes = {
+    books: PropTypes.array.isRequired
+  }
+  
   render() {
+    const { books } = this.props
+    
     return (
       <div className="list-books">
         <div className="list-books-title">
           <h1>MyReads</h1>
         </div>
+
         <div className="list-books-content">
           <div>
             <div className="bookshelf">
