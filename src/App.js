617b57c14a1bda44c1d33cb74f5cb6da44de58e7
Eduardo Molinari <emolinari@groupon.com>
2018-04-21 11:33:54 -0700

    adding restAPI call to getAll from BooksAPI

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then(books => { this.setState({books}) })
   }
 
   render() {
