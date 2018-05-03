c8ac5d7e5810de92ee58bed5337539e2db9589d6
Guilherme Revolta <guilhermerevolta@gmail.com>
2018-05-03 17:29:28 -0300

    feat: Create search component

---
+++ src/Search.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+class Search extends React.Component {
+  render() {
+    return(
+      <div className="search-books">
+        <div className="search-books-bar">
+          <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+          <div className="search-books-input-wrapper">
+            {/*
+              
+            */}
+            <input type="text" placeholder="Search by title or author"/>
+
+          </div>
+        </div>
+        <div className="search-books-results">
+          <ol className="books-grid"></ol>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Search;
