c17b1eb0f0a01ff0d9c56288c4ebcc2503a5a10f
Chris Camp <cpcamp2@gmail.com>
2018-05-24 14:48:26 -0400

    add starter component code to booklist and get book data from BooksAPI

--- src/App.js
+++ src/App.js
@@ -5,18 +5,20 @@ import './App.css'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then(data => {
+      this.setState({
+        books: data
+      })
+    })
   }
 
   render() {
     return (
-
+   <p>Hello hellllllllloooooo. YEAH YEAH YEAH</p>
     )
   }
 }
---
+++ src/BookList.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+
+class BookList extends Component {
+
+}
+
+export default BookList
