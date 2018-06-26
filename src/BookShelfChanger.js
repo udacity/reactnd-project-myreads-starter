8f2c16870951e3f8dd8a334fa7c39ee14be87eff
Fernando Limaverde <falstn@gmail.com>
2018-06-26 19:14:17 -0400

    Started componetization

--- src/BookShelfChanger.js
+++ src/BookShelfChanger.js
@@ -2,7 +2,15 @@ import React,{Component} from 'react'
 
 class BookShelfChanger extends Component {    
     render() {
-
+        return(
+            <select>
+            <option value="move" disabled>Move to...</option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None</option>
+          </select>
+          )
     }
 }
 
