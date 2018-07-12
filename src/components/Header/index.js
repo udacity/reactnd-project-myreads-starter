a92f5f9eaa890cde90f34c5d26ef4b0c015e1977
Alice <32536678+alicedxb@users.noreply.github.com>
2018-07-12 10:42:28 +0400

    Create index.js

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles.css';
+
+
+const Header = () => {
+	return (
+		<div className="list-books">
+			<div className="list-books-title">
+				<h1>MyReads</h1>
+			</div>
+		</div>
+	);
+}
+
+export default Header;
