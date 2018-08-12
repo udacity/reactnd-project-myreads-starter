520e6f13addb7b9a0aed63dfea2c51b25dda55ae
Ian Johnson <ianjohnson@Mac-Book-Pro.local>
2018-08-11 22:33:28 -0700

    fixed move option bug

--- src/Book.js
+++ src/Book.js
@@ -18,8 +18,8 @@ return(
 
            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.image})` }}></div>
            <div className="book-shelf-changer">
-             <select value={this.props.book.shelf || 'none' } onChange={change => this.handleChange( change.target.value)}>
-               <option value="none" disabled>Move to...</option>
+             <select value={this.props.book.shelf || 'none'} onChange={change => this.handleChange( change.target.value)}>
+               <option value="move" disabled>Move to...</option>
                <option value="currentlyReading">Currently Reading</option>
                <option value="wantToRead">Want to Read</option>
                <option value="read">Read</option>
