988fc8b4af83c9321504a6e7520a1120b989cdb8
Carlo Bruno <carlofbruno@gmail.com>
2018-08-29 21:09:48 -0700

    added gh-pages

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ class BooksApp extends React.Component {
   componentDidMount() {
     BooksAPI.getAll().then(books => {
       this.setState({ books: books });
-      console.log("State", this.state);
     });
   }
 
