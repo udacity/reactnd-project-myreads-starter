53a936c139cdebce182cf5f72f8a485d5d122158
William George Cook <wgeorgecook@gmail.com>
2018-09-11 14:18:54 -0700

    Search bar is own component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Router from 'react-router-dom'
 // import * as BooksAPI from './BooksAPI'
 import Bookshelf from './bookshelf.js'
+import SearchBar from './SearchBar.js'
 import './App.css'
 
 class BooksApp extends React.Component {
@@ -12,34 +13,16 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    showSearchPage: false
+    showSearchPage: true
   }
 
   render() {
     return (
       <div className="app">
         {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-        ) : (<Bookshelf />
+          <SearchBar />
+        ) : (
+          <Bookshelf />
         )}
       </div>
     )
