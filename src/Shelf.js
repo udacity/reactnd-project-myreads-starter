f29a87022444d466812c8abd5f5747ab69c38921
Fermin Blanco <luillyfe89@gmail.com>
2018-11-04 23:03:12 -0500

    style: formatting css, Shelf component

--- src/Shelf.js
+++ src/Shelf.js
@@ -17,16 +17,17 @@ class Shelf extends Component {
                                              width: 128,
                                              height: 193,
                                              backgroundImage: `url(${book.url})` }}>
+                                        <div className="book-shelf-changer">
+                                            <select>
+                                                <option value="move" disabled>Move to...</option>
+                                                <option value="currentlyReading">Currently Reading</option>
+                                                <option value="wantToRead">Want to Read</option>
+                                                <option value="read">Read</option>
+                                                <option value="none">None</option>
+                                            </select>
+                                        </div>
                                     </div>
-                                    <div className="book-shelf-changer">
-                                        <select>
-                                            <option value="move" disabled>Move to...</option>
-                                            <option value="currentlyReading">Currently Reading</option>
-                                            <option value="wantToRead">Want to Read</option>
-                                            <option value="read">Read</option>
-                                            <option value="none">None</option>
-                                        </select>
-                                    </div>
+
                                     <div className="book-title">{book.title}</div>
                                     <div className="book-authors">{book.author}</div>
                                 </div>
