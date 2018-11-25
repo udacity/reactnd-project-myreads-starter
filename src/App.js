843698cdd8760a64d3d1071726a8d9cff4c4f338
Anton Serdyuchenko <anton415@gmail.com>
2018-11-25 07:31:50 +0300

    Add state to a component.

--- src/App.js
+++ src/App.js
@@ -6,13 +6,8 @@ import ListBooks from './ListBooks';
 
 class BooksApp extends Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    showSearchPage: false,
+    showListBooks: true
   }
 
   render() {
--- src/Book.js
+++ src/Book.js
@@ -3,9 +3,8 @@ import './App.css'
 import BookShelfChanger from './BookShelfChanger'
 
 class Book extends Component {
-
-  render() {
-    let books = [
+  state = {
+    books: [
       {
         id: 0,
         backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")',
@@ -55,19 +54,21 @@ class Book extends Component {
         authors: 'Mark Twain',
         bookshelf_title: 'Read'
       }
-    ];
+    ]
+  }
 
+  render() {
     return (
       <ol className="books-grid">
-        {books.filter((book) => (book.bookshelf_title === this.props.bookshelf_title)).map((book) => (
+        {this.state.books.filter((book) => (book.bookshelf_title === this.props.bookshelf_title)).map((book) => (
           <li key={book.id}>
             <div className="book">
               <div className="book-top">
                 <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: book.backgroundImage}}></div>
                 <BookShelfChanger/>
               </div>
-              <div className="book-title">{book.title}</div>
-              <div className="book-authors">{books.authors}</div>
+              <div className="book-title">{this.state.books.title}</div>
+              <div className="book-authors">{this.state.books.authors}</div>
             </div>
           </li>
         ))}
