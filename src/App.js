32010aad5556173034acaf4bb0beaa577c9a5a0a
Anton Serdyuchenko <anton415@gmail.com>
2018-11-26 21:21:24 +0300

    Add link component.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import Book from './Book'
 import escapeRegExp from 'escape-string-regexp'
 import sortBy from 'sort-by'
+import {BrowserRouter} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 class BooksApp extends Component {
   componentDidMount() {
@@ -12,6 +14,7 @@ class BooksApp extends Component {
     })
   }
   state = {
+    screen: 'list', // list, search
     showSearchPage: false,
     query: '',
 
@@ -59,6 +62,8 @@ class BooksApp extends Component {
     this.setState((state) => ({
       books: newBooks
     }))
+    // Update in API
+    BooksAPI.update(book, selectBookshelfTitle)
   }
 
 
@@ -81,66 +86,77 @@ class BooksApp extends Component {
 
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
-              <div className="search-books-input-wrapper">
+        {this.state.screen === 'search' && (
+          <BrowserRouter>
+            <div className="search-books">
+              <div className="search-books-bar">
+                <Link to="/">
+                  <button className="close-search" onClick={() => this.setState({ screen: 'list' })}>Close</button>
+                </Link>
+                <div className="search-books-input-wrapper">
 
-                <input type="text" placeholder="Search by title or author"
-                  value={this.state.query}
-                  onChange={(event) => this.updateQuery(event.target.value)}
-                />
+                  <input type="text" placeholder="Search by title or author"
+                    value={this.state.query}
+                    onChange={(event) => this.updateQuery(event.target.value)}
+                  />
 
+                </div>
               </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid">
-                {showingBooks.map((book) => (
-                  <li key={book.id}>
-                    <div className="book">
-                      <div className="book-top">
-                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>
-                          <div  className="book-shelf-changer">
-                            <select value={book.bookshelf_title_value} onChange={(e) => this.props.onUpdateBookshelfTitle(book, e.target.value)} >
-                              <option value="move" disabled>Move to...</option>
-                              <option value="currentlyReading">Currently Reading</option>
-                              <option value="wantToRead">Want to Read</option>
-                              <option value="read">Read</option>
-                              <option value="none">None</option>
-                            </select>
-                          </div>
+              <div className="search-books-results">
+                <ol className="books-grid">
+                  {showingBooks.map((book) => (
+                    <li key={book.id}>
+                      <div className="book">
+                        <div className="book-top">
+                          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>
+                            <div  className="book-shelf-changer">
+                              <select value={book.bookshelf_title_value} onChange={(e) => this.props.onUpdateBookshelfTitle(book, e.target.value)} >
+                                <option value="move" disabled>Move to...</option>
+                                <option value="currentlyReading">Currently Reading</option>
+                                <option value="wantToRead">Want to Read</option>
+                                <option value="read">Read</option>
+                                <option value="none">None</option>
+                              </select>
+                            </div>
 
+                        </div>
+                        <div className="book-title">{book.title}</div>
+                        <div className="book-authors">{book.authors}</div>
                       </div>
-                      <div className="book-title">{book.title}</div>
-                      <div className="book-authors">{book.authors}</div>
-                    </div>
-                  </li>
-                ))}
-              </ol>
-            </div>
-          </div>
-        ) : (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                {this.bookshelf_titles.map((title) => (
-                  <div key={title.id} className="bookshelf">
-                    <h2 className="bookshelf-title">{title.name}</h2>
-                    <div className="bookshelf-books">
-                      <Book onUpdateBookshelfTitle={this.updateBookshelfTitle} bookshelf_title={title.value} books={this.state.books}/>
-                    </div>
-                  </div>
-                ))}
+                    </li>
+                  ))}
+                </ol>
               </div>
             </div>
-            <div className="open-search">
-              <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
+          </BrowserRouter>
+
+        )}
+        {this.state.screen === 'list' && (
+          <BrowserRouter>
+            <div className="list-books">
+              <div className="list-books-title">
+                <h1>MyReads</h1>
+              </div>
+              <div className="list-books-content">
+                <div>
+                  {this.bookshelf_titles.map((title) => (
+                    <div key={title.id} className="bookshelf">
+                      <h2 className="bookshelf-title">{title.name}</h2>
+                      <div className="bookshelf-books">
+                        <Book onUpdateBookshelfTitle={this.updateBookshelfTitle} bookshelf_title={title.value} books={this.state.books}/>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+              <div className="open-search">
+                <Link to="/search">
+
+                  <button onClick={() => this.setState({ screen: 'search' })}>Add a book</button>
+                </Link>
+              </div>
             </div>
-          </div>
+          </BrowserRouter>
         )}
       </div>
     )
