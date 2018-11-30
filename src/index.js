bb839d098f2c7c49e855cc9f3ead63c09075f5bf
V <artnerdnet@gmail.com>
2018-11-30 15:48:24 -0300

    created components

--- src/index.js
+++ src/index.js
@@ -2,14 +2,5 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
-// import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
-    document.getElementById('root')
-);
-
-// registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById('root'))
