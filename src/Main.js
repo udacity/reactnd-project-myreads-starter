a389358b07b9150138443ce8863dc1d4405b6150
nennenwodo <nennenwodo@gmail.com>
2018-12-02 07:45:48 +0100

    Fixed mistake in render books on the currently reading shelf. Added filter

--- src/Main.js
+++ src/Main.js
@@ -14,10 +14,13 @@ class Main extends Component {
             this.setState(() => ({
                 books
             }))
-            console.log(this.books)
+            //console.log(this.books)
         })
     }
     render() {
+        const currentlyReading = this.state.books.filter((book) => (
+            book.shelf === "currentlyReading"
+        ))
         return (
             <div className="app">
                 <div className="list-books">
@@ -30,7 +33,7 @@ class Main extends Component {
                                 <h2 className="bookshelf-title">Currently Reading</h2>
                                 <div className="bookshelf-books">
                                     <ol className="books-grid">
-                                    { this.state.books.map((book) => (
+                                    { currentlyReading.map((book) => (
                                         <li key = {book.id} >
                                             <div className="book">
                                                 <div className="book-top">
@@ -49,25 +52,7 @@ class Main extends Component {
                                                 <div className="book-authors">{book.authors.toString().replace(",", ", ")}</div>
                                             </div>
                                         </li>
-                                        ), this.state.books.shelf === "currentlyReading")} 
-                                        <li>
-                                            <div className="book">
-                                            <div className="book-top">
-                                                <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")' }}></div>
-                                                <div className="book-shelf-changer">
-                                                <select>
-                                                    <option value="move" disabled>Move to...</option>
-                                                    <option value="currentlyReading">Currently Reading</option>
-                                                    <option value="wantToRead">Want to Read</option>
-                                                    <option value="read">Read</option>
-                                                    <option value="none">None</option>
-                                                </select>
-                                                </div>
-                                            </div>
-                                            <div className="book-title">Ender's Game</div>
-                                            <div className="book-authors">Orson Scott Card</div>
-                                            </div>
-                                        </li>
+                                        ))} 
                                     </ol>
                                 </div>
                             </div>
