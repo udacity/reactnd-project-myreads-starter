a185e2197c618ce015f23f29291d9b2f355d822b
Ben Flores <beflore@microsoft.com>
2018-12-16 17:46:21 -0800

    Finish creating Bookshelf component

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Bookshelf extends React.Component {
   render() {
     // props destrucuring
-    const {title} = this.props;
+    const {title, books} = this.props;
 
     return (
       <div>
@@ -12,42 +12,32 @@ class Bookshelf extends React.Component {
           <h2 className="bookshelf-title">{title}</h2>
           <div className="bookshelf-books">
             <ol className="books-grid">
-              <li>
-                <div className="book">
-                  <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
-                    <div className="book-shelf-changer">
-                        <select>
-                        <option value="move" disabled>Move to...</option>
-                        <option value="currentlyReading">Currently Reading</option>
-                        <option value="wantToRead">Want to Read</option>
-                        <option value="read">Read</option>
-                        <option value="none">None</option>
-                        </select>
+
+              {/* MAP THROUGH LIST OF BOOKS, WHICH ARE PASSED VIA PROPS */}
+              {books.forEach( book => {
+                return (
+                  <li>
+                    <div className="book">
+                      <div className="book-top">
+                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.url})` }}></div>
+                        {/* Below can probably be made its own component */}
+                        <div className="book-shelf-changer">
+                          <select>
+                            <option value="move" disabled>Move to...</option>
+                            <option value="currentlyReading">Currently Reading</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                            <option value="none">None</option>
+                          </select>
+                        </div>
+                      </div>
+                      <div className="book-title">{book.title}</div>
+                      <div className="book-authors">{book.author}</div>
                     </div>
-                  </div>
-                  <div className="book-title">To Kill a Mockingbird</div>
-                  <div className="book-authors">Harper Lee</div>
-                </div>
-              </li>
-              <li>
-                <div className="book">
-                  <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")' }}></div>
-                    <div className="book-shelf-changer">
-                        <select>
-                        <option value="move" disabled>Move to...</option>
-                        <option value="currentlyReading">Currently Reading</option>
-                        <option value="wantToRead">Want to Read</option>
-                        <option value="read">Read</option>
-                        <option value="none">None</option>
-                        </select>
-                    </div>
-                  </div>
-                  <div className="book-title">Ender's Game</div>
-                  <div className="book-authors">Orson Scott Card</div>
-                </div>
-              </li>
+                  </li>
+                );
+              })}
+              
             </ol>
           </div>
         </div>
