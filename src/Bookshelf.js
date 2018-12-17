b618b5d4ae2bf5d9bdd4832507a54af1adf9648c
Ben Flores <beflore@microsoft.com>
2018-12-17 08:25:44 -0800

    Fix indentation in files

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -2,31 +2,31 @@ import React from 'react';
 import Book from './Book';
 
 class Bookshelf extends React.Component {
-  render() {
-    const {bookshelfTitle, books} = this.props;
+    render() {
+        const { bookshelfTitle, books } = this.props;
 
-    return (
-      <div>
-        <div className="bookshelf">
-          <h2 className="bookshelf-title">{bookshelfTitle}</h2>
-          <div className="bookshelf-books">
-            <ol className="books-grid">
-              {books && books.length > 0 && books.map( book => {
-                return (
-                  <li key={`${book.title}_${book.id}`}>
-                    <Book 
-                      book={book}
-                      onOptionChange={(book, value) => this.props.onOptionChange(book, value)}
-                    /> 
-                  </li>
-                );
-              })}
-            </ol>
-          </div>
-        </div>
-      </div>
-    );
-  }
+        return (
+            <div>
+                <div className="bookshelf">
+                    <h2 className="bookshelf-title">{bookshelfTitle}</h2>
+                    <div className="bookshelf-books">
+                        <ol className="books-grid">
+                            {books && books.length > 0 && books.map(book => {
+                                return (
+                                    <li key={`${book.title}_${book.id}`}>
+                                        <Book
+                                            book={book}
+                                            onOptionChange={(book, value) => this.props.onOptionChange(book, value)}
+                                        />
+                                    </li>
+                                );
+                            })}
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 };
 
 export default Bookshelf;
