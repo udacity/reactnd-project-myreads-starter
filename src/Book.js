ab73cf8e5cae2ca5e3538b95ca4a5ee6b41783ba
Ben Flores <beflore@microsoft.com>
2018-12-16 22:14:56 -0800

    Add null check for book thumbnail url

--- src/Book.js
+++ src/Book.js
@@ -8,7 +8,7 @@ class Book extends React.Component {
       <div>
         <div className="book">
           <div className="book-top">
-            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks && book.imageLinks.thumbnail})` }}></div>
             {/* Below can probably be made its own component */}
             <div className="book-shelf-changer">
               <select>
