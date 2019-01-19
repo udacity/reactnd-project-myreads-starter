97ec08324395a531a7c9a72dd056da04c52d4e59
artnerdnet@gmail.com <artnerdnet@gmail.com>
2019-01-19 21:40:35 +0100

    passing books to main

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -35,30 +35,6 @@ class SearchPage extends Component {
 	  }
 	};
 
-	// getResults = () => {
-	//   const { query } = this.state;
-	//   if (query) {
-	//     BooksAPI.search(query).then(results => {
-	//       if (results.error) {
-	//         return this.setState({ results: [] });
-	//       }
-	//       this.setState({ results });
-	//     });
-	//   } else {
-	//     this.setState({ results: [], query: "" });
-	//   }
-	// };
-
-	/*
-		updateShelf = (result, shelf) => {
-			BooksAPI.update(result, shelf).then(() => {
-				result.shelf = shelf;
-				const reorderedBooks = this.state.results
-					.filter(fb => fb.id !== result.id)
-					.splice([result]);
-				this.setState({ results: reorderedBooks });
-			});
-		};*/
 
 	render() {
 		return (
@@ -67,17 +43,6 @@ class SearchPage extends Component {
 					query = {this.state.query}
 					updateQuery={this.updateQuery}
 				/>
-				{/* <div className="search-books-bar">
-          <Link to="/" className="close-search">
-            Close
-          </Link>
-          <div className="search-books-input-wrapper">
-            <input
-              placeholder="Search for books by title or author"
-              value={this.state.query}
-              onChange={this.updateQuery}
-            /> 
-          </div>*/}
 				<div className="search-books-results">
 					<ol className="books-grid">
 					{this.state.results.filter(result => result.shelf !== 'none')
@@ -89,18 +54,6 @@ class SearchPage extends Component {
                 />
               </li>
             ))}
-						{/* {this.state.results.map(result => (
-							<li key={result.id}>
-								<Book
-									book={result}
-									updateShelf={this.props.updateShelf} 
-								/> 
-
-
-
-
-							</li>
-						))}*/}
 					</ol>
 				</div>
 			</div>
