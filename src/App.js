5a3c83a2bf82214d0a414b0fd9851112d3cb7e65
Jessica Grzanna <jgrzanna@me.com>
2019-04-29 17:23:15 +0200

    fix: Erase console.log for api call

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        {BooksAPI.getAll().then(data => console.log(data))}
         <Route path="/" component={Library} />
         <Route path="/search" component={BookSearch} />
       </div>
