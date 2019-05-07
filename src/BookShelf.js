3428333b02f19a4a36db8af7562d250b34e82fbc
Jessica Grzanna <jgrzanna@me.com>
2019-05-07 13:59:21 +0200

    refactor: Display pretty ceategory names
    Add css class for capitalizing words and display category for bookshelf with seperated words.

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -8,9 +8,16 @@ class BookShelf extends Component {
 
   render() {
     const { books, category } = this.props;
+
+    const readableCategory = category
+    .split(/(?=[A-Z])/)
+    .map((word, index) => {
+      return (<span className="capitalize" key={index}>{word}&nbsp;</span>)
+    });
+
     return (
       <div className="bookshelf">
-        <h2 className="bookshelf-title">{category}</h2>
+        <h2 className="bookshelf-title">{readableCategory}</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
             {books.map(book => (
