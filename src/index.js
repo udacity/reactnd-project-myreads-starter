c56d4aae0cdead25cd39eec9b1bbc9f3858da271
nokieng17 <nokieng17@gmail.com>
2019-05-22 09:38:32 +0700

    install react starter and required packages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import { BrowserRouter } from 'react-router-dom';
+
+ReactDOM.render(
+
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+    , document.getElementById('root')
+)
