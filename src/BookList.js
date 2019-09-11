c03d6e7c77296be3db6bc684d35e36af66051cb7
Soumya Veer <veer.soumya@gmail.com>
2019-09-10 22:03:22 -0700

    import components

--- src/BookList.js
+++ src/BookList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import BookListItem from "./BookListItem";
 
 class BookList extends Component {
   render() {
--- src/Section.js
+++ src/Section.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import BookList from "./BookList";
 
 class Section extends Component {
   state = {
