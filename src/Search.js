1b661d0d336f702d2153ef41653a8b85b001e385
Soumya Veer <veer.soumya@gmail.com>
2019-09-10 22:10:57 -0700

    Add SearchBar Components

--- src/Search.js
+++ src/Search.js
@@ -5,7 +5,7 @@ class Search extends Component {
   render() {
     return(
       <div>
-        
+
       </div>
     )
   }
---
+++ src/SearchBar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class SearchBar extends Component {
+  render(){
+    return(
+      <div>
+
+      </div>
+    )
+  }
+}
+
+
+export default SearchBar;
