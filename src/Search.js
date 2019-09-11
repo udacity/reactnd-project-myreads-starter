4eae9af8049e3ccdc1076366572a36a61b70d521
Soumya Veer <veer.soumya@gmail.com>
2019-09-10 22:08:49 -0700

    Add Search Component

---
+++ src/Search.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class Search extends Component {
+
+  render() {
+    return(
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Search;
