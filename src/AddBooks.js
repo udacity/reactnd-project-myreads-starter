d2928504dc49f5c3d04ceabacd0bdda6562db4e0
Daniel Brandt <dbbrandt@gmail.com>
2019-10-31 10:42:14 -0400

    Add component pages.

---
+++ src/AddBooks.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const AddBooks = ( {history}) => {
+  return(
+    <div className="search-books">
+      <div className="search-books-bar">
+        <button className="close-search" onClick={() => history.push('/')}>Close</button>
+        <div className="search-books-input-wrapper">
+          {/*
+                    NOTES: The search from BooksAPI is limited to a particular set of search terms.
+                    You can find these search terms here:
+                    https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+                    However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+                    you don't find a specific author or title. Every search is limited by search terms.
+                  */}
+          <input type="text" placeholder="Search by title or author"/>
+
+        </div>
+      </div>
+      <div className="search-books-results">
+        <ol className="books-grid"></ol>
+      </div>
+    </div>
+  )
+}
+
+export default AddBooks
--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
 /** 
  This course is not designed to teach Test Driven Development. 
@@ -8,9 +8,7 @@ import App from './App'
  is not required.
 **/
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-})
-
-
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<App />, div);
+});
