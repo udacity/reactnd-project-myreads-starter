03b9629cbd2831bf251761ddb103d1b053d0921f
Lacra <lacramioara.oloeriu@gmail.com>
2020-02-02 12:14:36 +0100

    add diMount component to see the list books

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
   state = {
+
 
     /**
      * TODO: Instead of using this state variable to keep track of which page
@@ -11,10 +12,22 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    showSearchPage: false
+    showSearchPage: false,
+    books:[]
+  }
+  componentDidMount(){
+    BooksAPI.getAll ()
+    .then((books)=>{
+      this.setState(() =>({
+        books
+      })
+      )
+    })
   }
 
   render() {
+    console.log("My message")
+    console.log (this.state.books)
     return (
       <div className="app">
         {this.state.showSearchPage ? (
