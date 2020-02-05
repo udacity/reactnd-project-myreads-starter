78edc3b9e6b19df78d8d002397fe75ed16793525
Lacra <lacramioara.oloeriu@gmail.com>
2020-02-05 22:01:42 +0100

    fixed the bookList major bug

---
+++ src/ShelfBooks.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class ShelfBooks extends React.Component {
+    render() {
+        const { bookList, allBooks } = this.props;
+        console.log('my shelf book marker')
+        console.log(bookList)
+
+
+        return (
+            <div>  place holder </div>
+        )
+    }
+
+}
+export default ShelfBooks
---
+++ src/Shelfs.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import ShelfBooks from './ShelfBooks'
+
+class Shelfs extends React.Component {
+    render() {
+        const { shelfKey, shelfNames, bookList, allBooks } = this.props;
+        console.log("my shelf")
+        console.log(bookList)
+        return (
+            <div className=' bookshelf'>
+                <h2 className='bookshelf-title' > {shelfNames}</h2>
+                <div>
+                    <ShelfBooks bookList={bookList}
+                        allBooks={allBooks}
+
+                    />
+                </div>
+
+            </div>
+        )
+
+    }
+
+}
+
+export default Shelfs
