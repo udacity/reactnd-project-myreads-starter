f479edb6092d0169ea01469378dea5fd35251056
Lacra <lacramioara.oloeriu@gmail.com>
2020-02-07 19:18:28 +0100

    Wire up BookItem

---
+++ src/BookItem.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+
+class BookItem extends React.Component {
+
+
+  render() {
+
+
+
+    return (
+
+      <div >
+        Hello from book item
+        </div>
+    )
+  }
+}
+
+export default BookItem
--- src/ShelfBooks.js
+++ src/ShelfBooks.js
@@ -1,16 +1,25 @@
 import React from 'react'
+import BookItem from './BookItem'
+
 
 class ShelfBooks extends React.Component {
     render() {
         const { bookList, allBooks } = this.props;
         console.log('my shelf book marker')
         console.log(bookList)
+        let bookItem = bookList.map( key=>{
+            return(
+                <BookItem />
+            )
+        })
+
+        
 
         return (
             <ol className="books-grid">
-                bookList
+                {bookItem}
             </ol>
-            
+
         )
     }
 
