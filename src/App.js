490f9359882842967aba1554ef3f99f9eb64238d
Lacra <lacramioara.oloeriu@gmail.com>
2020-02-08 20:50:15 +0100

    implemented rout based navigation

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import * as BooksAPI from './BooksAPI'
 import './App.css'
 import Shelfs from './Shelfs'
 import { Link } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 
 class BooksApp extends React.Component {
   state = {
@@ -69,19 +70,38 @@ class BooksApp extends React.Component {
 
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
+        <Route exact path='/' render={() => (
+          <div className="list-books">
+            <div className="list-books-title">
+              <h1>MyReads</h1>
+            </div>
+            <div className="list-books-content">
+              {shelves}
+            </div>
+
+            <div className="open-search">
+
+              <Link className="button"
+                to='/search'>
+                <button type="button">
+                  Add Book
+              </button></Link>
+            </div>
+          </div>
+        )} />
+        <Route path='/search' render={() => (
           <div className="search-books">
             <div className="search-books-bar">
               <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
               <div className="search-books-input-wrapper">
                 {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+                NOTES: The search from BooksAPI is limited to a particular set of search terms.
+                You can find these search terms here:
+                https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
 
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
+                However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+                you don't find a specific author or title. Every search is limited by search terms.
+              */}
                 <input type="text" placeholder="Search by title or author" />
 
               </div>
@@ -90,25 +110,9 @@ class BooksApp extends React.Component {
               <ol className="books-grid"></ol>
             </div>
           </div>
-        ) : (
-            <div className="list-books">
-              <div className="list-books-title">
-                <h1>MyReads</h1>
-              </div>
-              <div className="list-books-content">
-                {shelves}
-              </div>
 
-              <div className="open-search">
+        )} />
 
-                <Link className="button"
-                  to='/search'>
-                  <button type="button">
-                    Add Book
-                  </button></Link>
-              </div>
-            </div>
-          )}
       </div>
     )
   }
