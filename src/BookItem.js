1a8ff644f7f7cf0092e9fc1e4e505d601092786b
Bogdan <bogdan.oloeriu@gmail.com>
2020-02-09 14:11:01 +0100

    change shelf works for serarch. added getBookShelf function

--- src/BookItem.js
+++ src/BookItem.js
@@ -3,11 +3,27 @@ import React, { Component } from 'react'
 
 class BookItem extends Component {
 
+  getBookShelf(foundBook, allBooks){
+    let myBook
+    if (allBooks) {
+      myBook = allBooks.find(item => {
+        return item.id === foundBook.id
+      })
+    }
+
+    let isOwened = ((myBook !== undefined) ? true : false)
+
+    let shelfValue = isOwened ? myBook.shelf : "none"
+
+    return shelfValue
+
+  }
 
   render() {
     let { book , changeSelf, allBooks} = this.props
     console.log("all books")
     console.log(allBooks)
+
 
     return (
       <li>
@@ -15,7 +31,7 @@ class BookItem extends Component {
           <div className="book-top">
             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
             <div className="book-shelf-changer">
-              <select onChange={(e) =>{ changeSelf(book,e.target.value)}} value = {book.shelf}>
+              <select onChange={(e) =>{ changeSelf(book,e.target.value)}} value = {this.getBookShelf(book,allBooks)}>
                 <option value="move" disabled>Move to...</option>
                 <option value="currentlyReading">Currently Reading</option>
                 <option value="wantToRead">Want to Read</option>
