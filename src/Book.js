1f45f491fc0e17a1b85a76445d5368ae6310f070
susumoa <tobiasbori@gmail.com>
2020-02-28 21:57:13 +0000

    FIX: remove opacity change on shelfchange due to warning, DOCS: correct shelfchange part

--- src/Book.js
+++ src/Book.js
@@ -19,29 +19,17 @@ class Book extends Component {
 
     const onBookMove = (shelf) => {
       if (shelf !== undefined) {
-        this.setState({
-          changed: true
-        })
-        setTimeout(() => {
-          this.setState({
-            changed: false
-          })
-        }, 500)
+        this.setState((prevState) => ({
+          changed: !prevState.changed
+        }))
       }
-    }
-
-    let coverStyle = {opacity: 1}
-    if (this.state.changed) {
-      coverStyle = {boxShadow: "0 3px 4px 0 #dbedf3", opacity: 0.5}
     }
 
     return(
       <div className="book">
         <div className="book-top">
-          <div style={coverStyle}>
           <div className='book-cover' style={{width: 128, height: 193, backgroundImage: `url(${imageLinks !== undefined && imageLinks.thumbnail})`}}></div>
           <ShelfChanger book={this.props.book} myBooks={myBooks} shelf={shelf} onShelfChange={onShelfChange} onBookMove={onBookMove}/>
-          </div>
         </div>
         <div className="book-title">{title}</div>
         <div className="book-authors">{authors && authors.map((author) => <div key={author}>{author}</div>)}</div>
