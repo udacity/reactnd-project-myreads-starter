3a202ff14a5e2a4ed638ac795511ef8763400cf9
Kevin Becker <kevinfjbecker@gmail.com>
2020-03-03 23:21:12 +0100

    refactor: Extract BookShelfChanger
    Pull BookShelfChanger out as stateless functional component; import into
    Bookshelf. Naming is aligned with css class: book-shelf-changer.

---
+++ src/BookShelfChanger.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const BookShelfChanger = () => {
+    return (
+      <select>
+        <option value="move" disabled>Move to...</option>
+        <option value="currentlyReading">Currently Reading</option>
+        <option value="wantToRead">Want to Read</option>
+        <option value="read">Read</option>
+        <option value="none">None</option>
+      </select>
+    );
+  };
+
+export default BookShelfChanger;
