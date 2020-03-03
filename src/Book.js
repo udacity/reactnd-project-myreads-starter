d06dde4823b07c51978918ce236465f6d24d3d13
Kevin Becker <kevinfjbecker@gmail.com>
2020-03-04 00:54:26 +0100

    refactor: Extract Book
    Extract book as stateless functional component; this is a div not an list item. The property parameter is a book details object.
    
    Add a static array, "books", to Bookshelf. This is to support the static
    version "as-is". Bookshelf should be renamed; perhaps Bookrack or
    ListBooksContent. The former has better metaphorical value, while the
    latter conforms more strongly with the CSS naming convention.

---
+++ src/Book.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import BookShelfChanger from './BookShelfChanger';
+
+const Book = (props) => {
+    const { title, authors, cover } = props.bookDetails;
+    const { width, height, backgroundImage } = cover;
+    return (
+      <div className="book">
+        <div className="book-top">
+          <div className="book-cover" style={{ width: width, height: height, backgroundImage: backgroundImage }}></div>
+          <div className="book-shelf-changer">
+            <BookShelfChanger />
+          </div>
+        </div>
+        <div className="book-title">{title}}</div>
+        <div className="book-authors">{authors}}</div>
+      </div>
+    );
+  };
+
+export default Book;
