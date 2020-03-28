a84b1465ae8e7631c16d2ce8b0e891136f321f35
Samuel Will <samuelwill@linux.fritz.box>
2020-03-28 01:00:45 +0100

    Adding API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 // import * as BooksAPI from './BooksAPI'
 import './App.css';
+import Book from './Components/book';
+import * as API from './BooksAPI';
 
 class BooksApp extends React.Component {
   state = {
@@ -11,6 +13,22 @@ class BooksApp extends React.Component {
      * pages, as well as provide a good URL they can bookmark and share.
      */
     showSearchPage: false,
+    currentlyReading: [],
+    wantToRead: [],
+    Read: [],
+  };
+
+  addBook = (book, shelf) => {
+    console.log('Book:\n', book);
+    this.setState(prevState => {
+      shelf = prevState[shelf].push(book);
+    });
+  };
+  addInitialBooks;
+
+  componentDidMount = async () => {
+    const Books = await API.getAll();
+    Books.forEach(book => this.addBook(book, book.shelf));
   };
 
   render() {
@@ -53,32 +71,7 @@ class BooksApp extends React.Component {
                   <div className="bookshelf-books">
                     <ol className="books-grid">
                       <li>
-                        <div className="book">
-                          <div className="book-top">
-                            <div
-                              className="book-cover"
-                              style={{
-                                width: 128,
-                                height: 193,
-                                backgroundImage:
-                                  'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")',
-                              }}
-                            />
-                            <div className="book-shelf-changer">
-                              <select>
-                                <option value="move" disabled>
-                                  Move to...
-                                </option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                              </select>
-                            </div>
-                          </div>
-                          <div className="book-title">To Kill a Mockingbird</div>
-                          <div className="book-authors">Harper Lee</div>
-                        </div>
+                        <Book />
                       </li>
                       <li>
                         <div className="book">
@@ -107,6 +100,9 @@ class BooksApp extends React.Component {
                           <div className="book-title">Ender's Game</div>
                           <div className="book-authors">Orson Scott Card</div>
                         </div>
+                      </li>
+                      <li>
+                        <Book />
                       </li>
                     </ol>
                   </div>
