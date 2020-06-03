4063b9b914f8a3ee492c152a9e403b1bc3d4a5c0
Sudhir Kumar <2017ucp1296@mnit.ac.in>
2020-06-04 01:50:17 +0530

    updated main page

--- src/App.js
+++ src/App.js
@@ -6,16 +6,42 @@ import Search from './Search'
 import {BrowserRouter as Router , Route} from 'react-router-dom'
 
 class BooksApp extends React.Component {
-  
+
+  state = {
+    books : []
+  }
+
+  async componentDidMount()
+  {
+    this.updatebook();
+  }
+
+  async updatebook()
+  {
+    const fetchapi = BooksAPI.getAll();
+    fetchapi.then((data) =>
+    {
+      console.log(data);
+      this.setState({books : data});
+    });
+  }
+
+
+
 
   render() {
+
+    if(this.state.books.length === 0)
+    {
+      return <h1>loading ....</h1>
+    }
     return (
 
       <div>
         <Router>
           <Route path="/" exact render = { () =>
           (
-            <Main />
+            <Main books = {this.state.books}/>
           )}/>
           <Route path="/search" exact render = { () => (
             <Search />
