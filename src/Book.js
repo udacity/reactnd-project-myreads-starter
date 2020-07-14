ae9362a8762e0d7401f3ba8d44590bf6f799d40a
Tim Jenkins <jenks00@gmail.com>
2020-07-13 22:51:55 -0600

    fix: Show multiple authors in comma-separated list

--- src/Book.js
+++ src/Book.js
@@ -37,7 +37,7 @@ class Book extends React.Component {
             </div>
           </div>
           <div className="book-title">{book.title}</div>
-          <div className="book-authors">{book.authors}</div>
+          <div className="book-authors">{book.authors.join(', ')}</div>
         </div>
       </li>
     )
