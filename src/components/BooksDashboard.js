9897d2d140e7877ea38b746a59eca36fb8fa58eb
Asad Manzoor <asad33manzoor@gmail.com>
2020-08-02 01:07:58 +0500

    MyReads Project Submission

---
+++ src/components/BooksDashboard.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+
+import BooksShelf from './BooksShelf';
+import SearchInput from './SearchInput';
+
+const BooksDashboard = (props) => {
+    const {filterShelfBooks, handleShelfUpdate} = props;
+    return (
+        <div className="list-books">
+            <div className="list-books-title">
+                <h1>MyReads: A Book Lending App</h1>
+            </div>
+            <div className="list-books-content">
+                <div>
+                    <BooksShelf
+                        name='Currently Reading'
+                        books={filterShelfBooks('currentlyReading')}
+                        handleShelfUpdate={handleShelfUpdate}
+                    />
+                    <BooksShelf
+                        name='Want To Read'
+                        books={filterShelfBooks('wantToRead')}
+                        handleShelfUpdate={handleShelfUpdate}
+                    />
+                    <BooksShelf
+                        name='Read'
+                        books={filterShelfBooks('read')}
+                        handleShelfUpdate={handleShelfUpdate}
+                    />
+                </div>
+            </div>
+            <SearchInput />
+        </div>
+    )
+};
+
+BooksDashboard.propTypes = {
+    filterShelfBooks: PropTypes.func.isRequired,
+    handleShelfUpdate: PropTypes.func.isRequired
+};
+
+export default BooksDashboard
---
+++ src/components/BooksListing.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import BookItem from './BookItem'
+
+const BooksListing = (props) => {
+    const {books, handleShelfUpdate} = props;
+
+    const booksListing = books.map((book) => (
+        <BookItem key={book.id} book={book} handleShelfUpdate={handleShelfUpdate}/>
+    ));
+
+    return (
+        <ol className="books-grid">{booksListing}</ol>
+    )
+};
+
+BooksListing.propTypes = {
+    books: PropTypes.array.isRequired,
+    handleShelfUpdate: PropTypes.func.isRequired
+};
+
+export default BooksListing
