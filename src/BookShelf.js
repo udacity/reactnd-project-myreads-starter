0d30e1ef17111e3987404cc2d8877b18ae0ca651
VedranaR <vedrana_ravlic16@yahoo.com>
2021-02-26 00:35:46 +0100

    feat: separated book code into Book component

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Book from './Book';
 
 class BookShelf extends Component {
     state = {  }
@@ -7,32 +8,12 @@ class BookShelf extends Component {
         return ( 
             <div className="bookshelf">
                 <h2 className="bookshelf-title">{this.props.title}</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                     
-                    {books.map(book => ( //
-                        <li key={book.id}> 
-                            <div className="book">
-                            <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book["imageLinks"]["thumbnail"]})` }}></div>
-                            <div className="book-shelf-changer">
-                              <select>
-                                <option value="move" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                              </select>
-                            </div>
-                          </div>
-                          <div className="book-title">{book["title"]}</div>
-                          <div className="book-authors">{book["authors"].join(", ")}</div>
-                        </div>
-                      </li>
-                    ))}          
-                    </ol>
-                  </div>
+                <div className="bookshelf-books">
+                <ol className="books-grid">
+                    {books.map(book => (<Book book={book} key={book.id}/>))}        
+                </ol>
                 </div>
+            </div>
          );
     }
 }
