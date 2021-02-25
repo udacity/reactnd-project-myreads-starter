24c0b158bc0f4804e8ee3a09112d31aae14cda5e
VedranaR <vedrana_ravlic16@yahoo.com>
2021-02-26 00:12:11 +0100

    feat: implemented search functionality

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,22 +1,57 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import BookShelf from './BookShelf';
 
 class SearchBooks extends Component {
-    //state = {  }
-    render() { 
-        return ( 
-            <div className="search-books">
-            <div className="search-books-bar">
-              <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
-              <div className="search-books-input-wrapper">
-                <input type="text" placeholder="Search by title or author"/>
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
+state = {
+    query: ''
+}
 
-         );
+updateQuery = (query) => {
+    this.setState({ query: query.trim() })
+}
+
+render() { 
+    const { books } = this.props;
+    const { query } = this.state;
+
+    let showingBooks = books.filter(book => book["authors"].join().includes(query) || book["title"].includes(query));
+
+//console.log(showingBooks)
+//(title.charAt(0).toLowerCase() + title.slice(1)).split(' ').join('');
+    return ( 
+        <div className="search-books">
+            <div className="search-books-bar">
+            <Link 
+            to='/'
+            className="close-search"
+            >Close</Link>
+            <div className="search-books-input-wrapper">
+                <input 
+                type="text" 
+                placeholder="Search by title or author"
+                value={query}
+                onChange={(event) => this.updateQuery(event.target.value)}
+                />
+            </div>
+            </div>
+            {this.state.query && 
+            <div className="search-books-results">
+                <BookShelf 
+                    title = "Currently Reading"
+                    books={showingBooks.filter(book => book["shelf"] === "currentlyReading")}
+                />
+                <BookShelf 
+                    title = "Want To Read"
+                    books={showingBooks.filter(book => book["shelf"] === "wantToRead")}
+                />
+                <BookShelf 
+                    title = "Read"
+                    books={showingBooks.filter(book => book["shelf"] === "read")}
+                />
+            </div>}
+        </div>
+        );
     }
 }
  
