696d6752e6098885bd98515364c4f06b4f1298c3
VedranaR <vedrana_ravlic16@yahoo.com>
2021-03-29 00:09:51 +0200

    feat: show selected shelf in submenu 4 every book

--- src/Book.js
+++ src/Book.js
@@ -11,13 +11,13 @@ const {book} = this.props
         <div className="book-top">
         <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book["imageLinks"]["thumbnail"]})` }}></div>
         <div className="book-shelf-changer">
-          <select>
+          <select value = {book.shelf}>
             <option value="move" disabled>Move to...</option>
             <option value="currentlyReading">Currently Reading</option>
             <option value="wantToRead">Want to Read</option>
             <option value="read">Read</option>
             <option value="none">None</option>
-          </select>
+          </select> 
         </div>
         </div>
         <div className="book-title">{book["title"]}</div>
