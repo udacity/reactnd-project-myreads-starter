2a2ff06bfa18725c50e5cc6ee47884081b91344c
sophia <sophieoyiza@gmail.com>
2021-07-08 05:50:36 +0100

    feat: :sparkles: upsates react version to use hooks +  components
    updates react version to use hooks + adds components
    
    none

--- src/index.js
+++ src/index.js
@@ -3,4 +3,11 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import { BrowserRouter } from 'react-router-dom';
+
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+
+document.getElementById('root'));
