04493e6f6a51a0510e1096d6666eccd15628563c
Bismark Atta FRIMPONG <onuapaye@yahoo.com>
2021-08-04 08:18:40 +0200

    segrated Apps.js into inidividual components

---
+++ src/components/BookListing.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class BookListing extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                
+            </div>
+         );
+    }
+}
+ 
+export default BookListing;
---
+++ src/components/CurrentlyReading.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+
+class CurrentlyReading extends Component {
+    state = {}
+    render() {
+        return (
+            <div className="bookshelf">
+                <h2 className="bookshelf-title">{this.props.bookshelfTitle}</h2>
+                <div className="bookshelf-books">
+                    <ol className="books-grid">
+                        <li>
+                            <div className="book">
+                                <div className="book-top">
+                                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+                                    <div className="book-shelf-changer">
+                                        <select>
+                                            <option value="move" disabled>Move to...</option>
+                                            <option value="currentlyReading">Currently Reading</option>
+                                            <option value="wantToRead">Want to Read</option>
+                                            <option value="read">Read</option>
+                                            <option value="none">None</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div className="book-title">To Kill a Mockingbird</div>
+                                <div className="book-authors">Harper Lee</div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="book">
+                                <div className="book-top">
+                                    <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")' }}></div>
+                                    <div className="book-shelf-changer">
+                                        <select>
+                                            <option value="move" disabled>Move to...</option>
+                                            <option value="currentlyReading">Currently Reading</option>
+                                            <option value="wantToRead">Want to Read</option>
+                                            <option value="read">Read</option>
+                                            <option value="none">None</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div className="book-title">Ender's Game</div>
+                                <div className="book-authors">Orson Scott Card</div>
+                            </div>
+                        </li>
+                    </ol>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default CurrentlyReading;
