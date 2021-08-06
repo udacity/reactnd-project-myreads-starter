8b8f3bc0ac1170023cf8341e8b9eb3c39d1ba28a
Bismark Atta FRIMPONG <onuapaye@yahoo.com>
2021-08-06 18:07:53 +0200

    modified the search page logic

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -10,7 +10,6 @@ class SearchPage extends Component {
     };
 
     handleOnChange = (searchWord) => {
-        //event.persist()
         this.setState(() => ({
             searchText: searchWord.trim()
         }))
@@ -30,33 +29,57 @@ class SearchPage extends Component {
     }
 
     render() {
+        //let newBook = [];
         const { tempSearchResults, searchText } = this.state;
         // const displaySearchResults = (searchText) === ''
         //     ? []
-        //     : tempSearchResults.map((bookElement, index) => (bookElement[index])).filter((book) => 
-        //         book.title.toLowerCase().includes(searchText.toLowerCase())
+        //     : tempSearchResults.filter(o => o.)
+        //     // : tempSearchResults.map((bookElement, index) => {
+        //     //         console.log('ELEMENT', bookElement[index])
+        //     //         return bookElement[index]
+        //     //     }).filter((book) => {
+        //     //     if (typeof book.title !== 'undefined') {
+        //     //         newBook.push(book)
+        //     //         console.log('BOOKS', newBook)
+        //     //        return book.title.toLowerCase().includes(searchText.toLowerCase())
+        //     //     }
+        //     //     return newBook
+        //     // }
         //     )
-
-        const showSearchResult = () => {
-            if (searchText === '') {
-                return []
-            } else  {
-                try {
-                    const mappedBook = tempSearchResults.map((e, i) => (e[i]))
-    
-                    const filteredBook = mappedBook.filter((book) => {
-                        if ((typeof book.title !== 'undefined') && (typeof book.authors !== 'undefined') && (typeof book.imageLinks !== 'undefined')) {
-                            return book.title.toLowerCase().includes(searchText.toLowerCase())
-                        }
-                        return null
-                    })
-
-                    return filteredBook
-                } catch (error) {
-                    console.log('Error', error)
-                }
+        const handleDisplay = () => {
+            let book = [];
+            for (const bookData of tempSearchResults) {
+                Object.keys(bookData).filter((index) => {
+                    if(typeof bookData[index].title !== 'undefined'){
+                        book.push(bookData[index])
+                        return bookData[index].title.toLowerCase().includes(searchText.toLowerCase())
+                    }
+                    return null;
+                })
             }
+            return book
         }
+
+        // const showSearchResult = () => {
+        //     if (searchText === '') {
+        //         return []
+        //     } else  {
+        //         try {
+        //             const mappedBook = tempSearchResults.map((e, i) => (e[i]))
+
+        //             const filteredBook = mappedBook.filter((book) => {
+        //                 if ((typeof book.title !== 'undefined') && (typeof book.authors !== 'undefined') && (typeof book.imageLinks !== 'undefined')) {
+        //                     return book.title.toLowerCase().includes(searchText.toLowerCase())
+        //                 }
+        //                 return null
+        //             })
+
+        //             return filteredBook
+        //         } catch (error) {
+        //             console.log('Error', error)
+        //         }
+        //     }
+        // }
 
         return (
             <div className="search-books">
@@ -64,23 +87,24 @@ class SearchPage extends Component {
                     <Link to='/'>
                         <button className="close-search">Close</button>
                     </Link>
-                    
+
                     <div className="search-books-input-wrapper">
                         <input
                             type="text"
                             placeholder="Search by title or author"
-                            value={searchText}
+                            defaultValue={searchText}
                             onChange={(event) => this.handleOnChange(event.target.value)} />
                     </div>
                 </div>
                 <div className="search-books-results">
                     <ol className="books-grid">
                         {
-                            Array.isArray(showSearchResult()) ? showSearchResult().map((result, index) =>
+                            searchText.length > 0 ? handleDisplay().map((result, index) =>
                                 <li key={index} >
                                     <div className="book">
                                         <div className="book-top">
-                                            <div className="book-cover" style={{ width: 128, height: 174, backgroundImage: `url(${result.imageLinks ? result.imageLinks.thumbnail : ''})` }}></div>
+                                            <div className="book-cover"
+                                                style={{ width: 128, height: 174, backgroundImage: `url(${result.imageLinks ? result.imageLinks.thumbnail : ''})` }}></div>
                                             <div className="book-shelf-changer">
                                                 <BookOptions />
                                             </div>
