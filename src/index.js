33b706605611280041288d40069a061a78eafb9d
Gustavo Moreira <gustavo1995.2012@hotmail.com>
2021-10-17 12:34:23 +0100

    Added registerServiceWorker.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker();
