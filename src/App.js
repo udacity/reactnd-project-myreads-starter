8f34a558f3fd92026ec463d4f26434794c67c53c
Arthur Harduim <arthur.harduim@rioenergy.com.br>
2021-12-29 00:35:14 -0300

    Feat: get book shellfs from API

--- src/App.js
+++ src/App.js
@@ -5,33 +5,6 @@ import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 
-const sampleReading = [
-  {
-    title: "To Kill a Mockingbird",
-    authors: "Harper Lee",
-    coverUrl: "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api"
-  },
-  {
-    title: "Ender's Game",
-    authors: "Orson Scott Card",
-    coverUrl: "http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api"
-  }
-]
-
-const sampleWant = [
-  {
-    title: "1776",
-    authors: "David McCullough",
-    coverUrl: "http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api"
-  },
-  {
-    title: "Harry Potter and the Sorcerer's Stone",
-    authors: "J.K. Rowling",
-    coverUrl: "http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api"
-  }
-]
-
-
 class SearchPage extends Component {
   state = { booksFound: [], query: "", allBooks: [] }
 
@@ -39,7 +12,7 @@ class SearchPage extends Component {
     if (query != "") {
       return BooksAPI.search(query).then(books => this.setState({ booksFound: books, query: query.trimStart() }))
     }
-    return this.setState({ query: query.trimStart(), booksFound:[] })
+    return this.setState({ query: query.trimStart(), booksFound: [] })
   }
 
   render() {
@@ -107,7 +80,7 @@ class BookShelf extends Component {
         <div className="bookshelf-books">
           <ol className="books-grid">
             {this.props.bookList.map((book) => {
-              return <li><Book title={book.title} authors={book.authors} cover={book.coverUrl} /></li>
+              return <li><Book title={book.title} authors={book.authors} cover={book.imageLinks.thumbnail} /></li>
             })}
           </ol>
         </div>
@@ -146,7 +119,21 @@ class BooksPage extends Component {
 
 
 class BooksApp extends Component {
-  state = { reading: sampleReading, wantRead: sampleWant, read: sampleWant }
+  state = { currentlyReading: [], wantToRead: [], read: [] }
+
+  componentDidMount() {
+    BooksAPI.getAll().then(
+      books => this.setState(
+        (_) => {
+          return {
+            currentlyReading: books.filter(b => b.shelf == "currentlyReading"),
+            wantToRead: books.filter(b => b.shelf == "wantToRead"),
+            read: books.filter(b => b.shelf == "read"),
+          }
+        }
+      )
+    )
+  }
 
   addReading(book) {
     return
@@ -162,10 +149,11 @@ class BooksApp extends Component {
 
 
   render() {
+    const {currentlyReading, wantToRead, read} = this.state;
     return (
       <Routes>
         <Route exact path="/" element={
-          <BooksPage reading={sampleReading} wantRead={sampleWant} read={sampleReading} />
+          <BooksPage reading={currentlyReading} wantRead={wantToRead} read={read} />
         } />
         <Route exact path="/search" element={<SearchPage />} />
       </Routes>
