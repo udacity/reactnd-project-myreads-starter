9efe28a6fcfa4b56a439e738ce3c0e3ecf8f25bc
Arthur Harduim <arthur.harduim@rioenergy.com.br>
2021-12-29 13:02:10 -0300

    Add logo and favicon

--- src/components/BooksPage.js
+++ src/components/BooksPage.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
 import BookShelf from './BookShelf'
 import { Link } from 'react-router-dom'
+import logo from '../assets/logo.png'
 
 class BooksPage extends Component {
-  render () {
+  render() {
     return (
       <div className='app'>
         <div className='list-books'>
           <div className='list-books-title'>
-            <h1>My Reads</h1>
+            <h1><img src={logo} alt='MyReads Logo' style={{ height: "2rem", verticalAlign: "middle" }} /> My Reads</h1>
           </div>
           <div className='list-books-content'>
             <div>
