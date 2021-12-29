8292a05b6395ea97dbc408d9b62831a88873f3d7
Arthur Harduim <arthur.harduim@rioenergy.com.br>
2021-12-29 10:10:52 -0300

    Chore: split components into files

---
+++ src/components/BookShelf.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import Book from './Book'
+
+
+class BookShelf extends Component {
+    render() {
+      return (
+        <div className="bookshelf">
+          <h2 className="bookshelf-title">{this.props.title}</h2>
+          <div className="bookshelf-books">
+            <ol className="books-grid">
+              {this.props.bookList.map((book) => {
+                return <li key={book.id}><Book book={book} updadeShelf={this.props.updadeShelf} /></li>
+              })}
+            </ol>
+          </div>
+        </div>
+      )
+    }
+  }
+
+export default BookShelf;
---
+++ src/components/SearchPage.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import { DebounceInput } from 'react-debounce-input';
+import { Link } from "react-router-dom";
+import Book from './Book';
+import * as BooksAPI from '../BooksAPI'
+
+
+class SearchPage extends Component {
+    state = { booksFound: [], query: "", allBooks: [] }
+
+    getBooks(query) {
+        if (query !== "") {
+            return BooksAPI.search(query).then(books => this.setState({ booksFound: books, query: query.trimStart() }))
+        }
+        return this.setState({ query: query.trimStart(), booksFound: [] })
+    }
+
+    render() {
+        const { query } = this.state
+
+        return (
+            <div className="search-books">
+                <div className="search-books-bar">
+                    <Link to="/"><button className="close-search">Close</button></Link>
+                    <div className="search-books-input-wrapper">
+                        <DebounceInput
+                            type="text"
+                            minLength={0}
+                            debounceTimeout={300}
+                            onChange={(event) => this.getBooks(event.target.value)}
+                            placeholder="Search by title or author"
+                            value={query} />
+                    </div>
+                </div>
+                <div>
+                </div>
+                <div className="search-books-results">
+                    <ol className="books-grid">
+                        {
+                            this.state.booksFound.length > 0
+                                ? this.state.booksFound.map(b => <li key={b.id}><Book book={b} updadeShelf={this.props.updadeShelf} /></li>)
+                                : <div></div>
+                        }
+                    </ol>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default SearchPage;
