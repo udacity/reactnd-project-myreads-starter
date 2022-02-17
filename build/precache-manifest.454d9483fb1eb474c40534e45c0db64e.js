eebd81fda1d0e04c3fef542658e2301ab8f17969
izhang <izhang@seisware.com>
2022-02-17 12:34:47 -0700

    update yarn

---
+++ build/precache-manifest.454d9483fb1eb474c40534e45c0db64e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d558c5ac13cc6e0ea9b",
+    "url": "/static/css/main.dd1d2500.chunk.css"
+  },
+  {
+    "revision": "1d558c5ac13cc6e0ea9b",
+    "url": "/static/js/main.1d558c5a.chunk.js"
+  },
+  {
+    "revision": "0dee416a0983164f933d",
+    "url": "/static/js/1.0dee416a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "72a3c27d4822c33c044f1303ead5cc91",
+    "url": "/static/media/bg.72a3c27d.jfif"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "714cbb024c27d0f3507a2dd0d7c00908",
+    "url": "/index.html"
+  }
+];
