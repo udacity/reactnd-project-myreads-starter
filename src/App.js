71f61774909dd213e1c3075a5464d6037cf4ad44
Mina Tofa <67973256+MaverikTofa@users.noreply.github.com>
2022-04-09 14:13:05 +0400

    convert app component to functional component
    using useState hook to control the state for now

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+
 // import * as BooksAPI from './BooksAPI'
 import "./App.css";
 import Book from "./components/Book";
 
-class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false,
-  };
+const App = () => {
+  // state = {
+  //   /**
+  //    * TODO: Instead of using this state variable to keep track of which page
+  //    * we're on, use the URL in the browser's address bar. This will ensure that
+  //    * users can use the browser's back and forward buttons to navigate between
+  //    * pages, as well as provide a good URL they can bookmark and share.
+  //    */
+  //   showSearchPage: false,
+  // };
+  const [showSearchPage, setShowSearchPage] = useState(false);
 
-  render() {
-    return (
-      <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <button
-                className="close-search"
-                onClick={() => this.setState({ showSearchPage: false })}
-              >
-                Close
-              </button>
-              <div className="search-books-input-wrapper">
-                {/*
+  return (
+    <div className="app">
+      {showSearchPage ? (
+        <div className="search-books">
+          <div className="search-books-bar">
+            <button
+              className="close-search"
+              onClick={() => setShowSearchPage(false)}
+            >
+              Close
+            </button>
+            <div className="search-books-input-wrapper">
+              {/*
                   NOTES: The search from BooksAPI is limited to a particular set of search terms.
                   You can find these search terms here:
                   https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
@@ -35,74 +36,71 @@ class BooksApp extends React.Component {
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
-                <input type="text" placeholder="Search by title or author" />
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
+              <input type="text" placeholder="Search by title or author" />
             </div>
           </div>
-        ) : (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Currently Reading</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      <li>
-                        <Book />
-                      </li>
-                      <li>
-                        <Book />
-                      </li>
-                    </ol>
-                  </div>
+          <div className="search-books-results">
+            <ol className="books-grid"></ol>
+          </div>
+        </div>
+      ) : (
+        <div className="list-books">
+          <div className="list-books-title">
+            <h1>MyReads</h1>
+          </div>
+          <div className="list-books-content">
+            <div>
+              <div className="bookshelf">
+                <h2 className="bookshelf-title">Currently Reading</h2>
+                <div className="bookshelf-books">
+                  <ol className="books-grid">
+                    <li>
+                      <Book />
+                    </li>
+                    <li>
+                      <Book />
+                    </li>
+                  </ol>
                 </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Want to Read</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      <li>
-                        <Book />
-                      </li>
-                      <li>
-                        <Book />
-                      </li>
-                    </ol>
-                  </div>
+              </div>
+              <div className="bookshelf">
+                <h2 className="bookshelf-title">Want to Read</h2>
+                <div className="bookshelf-books">
+                  <ol className="books-grid">
+                    <li>
+                      <Book />
+                    </li>
+                    <li>
+                      <Book />
+                    </li>
+                  </ol>
                 </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Read</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      <li>
-                        <Book />
-                      </li>
-                      <li>
-                        <Book />
-                      </li>
-                      <li>
-                        <Book />
-                      </li>
-                    </ol>
-                  </div>
+              </div>
+              <div className="bookshelf">
+                <h2 className="bookshelf-title">Read</h2>
+                <div className="bookshelf-books">
+                  <ol className="books-grid">
+                    <li>
+                      <Book />
+                    </li>
+                    <li>
+                      <Book />
+                    </li>
+                    <li>
+                      <Book />
+                    </li>
+                  </ol>
                 </div>
               </div>
             </div>
-            <div className="open-search">
-              <button onClick={() => this.setState({ showSearchPage: true })}>
-                Add a book
-              </button>
-            </div>
           </div>
-        )}
-      </div>
-    );
-  }
-}
+          <div className="open-search">
+            <button onClick={() => setShowSearchPage(true)}>Add a book</button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default BooksApp;
+export default App;
