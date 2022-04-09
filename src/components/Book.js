6ecba3f87f0844111ef2e7b1eccff1f2037d1e49
Mina Tofa <67973256+MaverikTofa@users.noreply.github.com>
2022-04-09 12:05:52 +0400

    created a book component

---
+++ src/components/Book.js
@@ -0,0 +1,32 @@
+const Book = () => {
+  return (
+    <div className="book">
+      <div className="book-top">
+        <div
+          className="book-cover"
+          style={{
+            width: 128,
+            height: 188,
+            backgroundImage:
+              'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")',
+          }}
+        ></div>
+        <div className="book-shelf-changer">
+          <select>
+            <option value="move" disabled>
+              Move to...
+            </option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None</option>
+          </select>
+        </div>
+      </div>
+      <div className="book-title">Ender's Game</div>
+      <div className="book-authors">Orson Scott Card</div>
+    </div>
+  );
+};
+
+export default Book;
